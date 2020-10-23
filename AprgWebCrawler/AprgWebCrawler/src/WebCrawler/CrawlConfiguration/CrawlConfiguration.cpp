64d5e1027a7c944d41dd8a87fbf32cda21606938
mevalba <markearvinalba@gmail.com>
2020-10-23 19:04:28 +0800

    Modify aprgwebcrawler filesize limits

--- AprgWebCrawler/AprgWebCrawler/src/WebCrawler/CrawlConfiguration/CrawlConfiguration.cpp
+++ AprgWebCrawler/AprgWebCrawler/src/WebCrawler/CrawlConfiguration/CrawlConfiguration.cpp
@@ -31,15 +31,13 @@ CrawlConfiguration::CrawlConfiguration(CrawlMode mode)
     case CrawlMode::Y8:
         m_isFileToBeDownloadUntilSuccessful=true;
         m_numberOfRetries = 0;
-        m_minimumFileSize = 10000;
+        m_minimumFileSize = 1000;
         break;
     case CrawlMode::Youtube:
-        m_isFileToBeDownloadUntilSuccessful=true;
-        m_numberOfRetries = 0;
+        m_isFileToBeDownloadUntilSuccessful=true;        m_numberOfRetries = 0;
         m_minimumFileSize = 1000000;
         break;
-    case CrawlMode::Empty:
-    case CrawlMode::Unknown:
+    case CrawlMode::Empty:    case CrawlMode::Unknown:
         break;
     }
 }
