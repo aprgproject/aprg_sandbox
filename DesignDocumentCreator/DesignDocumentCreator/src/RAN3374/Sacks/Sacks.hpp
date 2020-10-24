0cba514c77982594574a2e89bcbba1111ea28d8a
mevalba <markearvinalba@gmail.com>
2020-10-24 08:13:23 +0800

    Remove RAN3374 sacks in designdocumentcreator

--- DesignDocumentCreator/DesignDocumentCreator/src/RAN3374/Sacks/Sacks.hpp
+++ DesignDocumentCreator/DesignDocumentCreator/src/RAN3374/Sacks/Sacks.hpp
@@ -1,36 +1,6 @@
-#include <Common/Sacks/SupplementarySacks.hpp>
-#include <RAN3374/Sacks/SupplementarySacks.hpp>
+#include <glo_def.h>
 
 typedef u32 TAntennaCarrierAPIDataUnitId;
 typedef u32 TAntennaCarrierAPITransactionId;
 typedef i32 TAntennaCarrierMonitoringId;
 typedef i32 TAntennaCarrierOneHundredthOf_dBm;
-
-struct AntennaCarrier_PowerEventInd
-{
-    TAntennaCarrierAPIDataUnitId                 discriminator;
-    TAntennaCarrierAPITransactionId              transaction;
-    UAntennaCarrierPowerEventCharacteristics     characteristics;
-    TAntennaCarrierMonitoringId                  id;
-    TAntennaCarrierOneHundredthOf_dBm            power;
-};
-
-struct BbRakePowerReport
-{
-    u32 power;
-};
-
-struct SHwConfigurationMsg
-{
-    SMessageAddress         btsomTelecomServiceAddr;
-    SWamUnit                wamUnit[MAX_NUM_OF_CONTROL_UNITS];
-    SWspUnit                wspUnit[MAX_NUM_OF_DSP_RESOURCES];
-    SCarrierResources       carrierResources;
-    TNumberOfItems          numOfValidLcr;
-    SLocalCellResource      localCellResource[MAX_NUM_OF_LOCAL_CELLS];
-    SBsConfigData           bsConfigData;
-    TNumberOfItems          btsSwVersionLength;
-    TDynamicData            btsSwVersion[1];
-};
-
-
