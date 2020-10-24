5981ebe4ac6ff9806f1f575343c34767bdc60118
mevalba <markearvinalba@gmail.com>
2020-10-24 17:44:15 +0800

    Implement SimplestDesignToCopyForLibraries

---
+++ SimplestDesignToCopyForLibraries/SimplestDesignToCopyForLibraries/src/main.cpp
@@ -0,0 +1,8 @@
+#include <SimplestDesignToCopyForLibraries.hpp>
+
+using namespace alba;
+
+int main()
+{
+    return 0;
+}
---
+++ SimplestDesignToCopyForLibraries/SimplestDesignToCopyForLibraries/tst/SimplestDesignToCopyForLibraries_unit.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <SimplestDesignToCopyForLibraries.hpp>
+
+using namespace alba;
+using namespace std;
+
+TEST(SampleTest, DISABLED_SampleTest1)
+{
+    SimplestDesignToCopyForLibraries entity;
+}
