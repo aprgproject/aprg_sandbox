e6e43e69201edfe9917b135da2f6772fc94df275
mevalba <markearvinalba@gmail.com>
2020-10-24 16:12:31 +0800

    Introduce btstracedecoder

---
+++ BtsTraceDecoder/BtsTraceDecoder/tst/BtsTraceDecoder_unit.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <SimplestDesignToCopy.hpp>
+
+using namespace alba;
+using namespace std;
+
+TEST(SampleTest, DISABLED_SampleTest1)
+{
+    SimplestDesignToCopy entity;
+}
