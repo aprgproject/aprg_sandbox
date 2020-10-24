497bcfc135c673383a06bc36c80a31df3f47e56d
mevalba <markearvinalba@gmail.com>
2020-10-24 18:52:47 +0800

    Add meansquarecalculator

---
+++ MeanSquareErrorCalculator/MeanSquareErrorCalculator/src/main.cpp
@@ -0,0 +1,11 @@
+#include <UserInterface.hpp>
+
+using namespace mse;
+using namespace std;
+
+int main()
+{
+    cout<<"Mean Square Error Calculator"<<endl;
+    UserInterface ui;
+    ui.execute();
+}
