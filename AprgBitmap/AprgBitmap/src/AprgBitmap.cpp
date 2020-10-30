dbe8994e7fb22206d2a7a9fdde45747a9a351d0f
mevalba <markearvinalba@gmail.com>
2020-10-30 08:26:53 +0800

    Integrate functionalities into aprgbitmap

--- AprgBitmap/AprgBitmap/src/AprgBitmap.cpp
+++ AprgBitmap/AprgBitmap/src/AprgBitmap.cpp
@@ -11,14 +11,11 @@
 
 using namespace std;
 
-
 namespace alba
 {
-
 AprgBitmap::XY::XY()
     : x(0)
-    , y(0)
-{}
+    , y(0){}
 
 AprgBitmap::XY::XY(unsigned int xValue, unsigned int yValue)
     : x(xValue)
@@ -71,37 +68,44 @@ void AprgBitmap::RectangularPixelsSnippet::addPosition(AprgBitmap::XY const posi
     }
 }
 
-void AprgBitmap::RectangularPixelsSnippet::clear()
+void AprgBitmap::RectangularPixelsSnippet::setCorners(AprgBitmap::XY const topLeftCornerPosition, AprgBitmap::XY const bottomRightCornerPosition)
 {
-    pixels.clear();
-    noPositionIsAddedYet = true;
+    topLeftCorner = topLeftCornerPosition;
+    bottomRightCorner = bottomRightCornerPosition;
 }
 
+void AprgBitmap::RectangularPixelsSnippet::clear()
+{
+    pixelData.clear();
+    noPositionIsAddedYet = true;
+}
 unsigned int AprgBitmap::RectangularPixelsSnippet::getDeltaX() const
 {
-    return bottomRightCorner.x - topLeftCorner.x;
-}
+    return bottomRightCorner.x - topLeftCorner.x;}
 
 unsigned int AprgBitmap::RectangularPixelsSnippet::getDeltaY() const
 {
     return bottomRightCorner.y - topLeftCorner.y;
 }
 
-bool AprgBitmap::RectangularPixelsSnippet::isPositionInsideTheSnippet(AprgBitmap::XY const position) const
+unsigned int AprgBitmap::RectangularPixelsSnippet::getNumberOfPixelsInSnippet() const
 {
-    return topLeftCorner.x <= position.x && topLeftCorner.y <= position.y && bottomRightCorner.x >= position.x && bottomRightCorner.y >= position.y;
+    return getDeltaX()*getDeltaY();
 }
 
+bool AprgBitmap::RectangularPixelsSnippet::isPositionInsideTheSnippet(AprgBitmap::XY const position) const
+{
+    return topLeftCorner.x <= position.x && topLeftCorner.y <= position.y && bottomRightCorner.x >= position.x && bottomRightCorner.y >= position.y;}
+
 AprgBitmap::AprgBitmap(string const& path)
-    : m_path(AlbaLocalPathHandler(path).getFullPath())
+    : m_maximumMemoryConsumption(DEFAULT_MAXIMUM_MEMORY_CONSUMPTION)
+    , m_path(AlbaLocalPathHandler(path).getFullPath())
 {
     loadBitmapHeaders();
 }
-
 bool AprgBitmap::isValid() const
 {
-    return isSignatureValid() && isHeaderValid() && isNumberOfColorPlanesValid() && isNumberOfBitsPerPixelValid();
-}
+    return isSignatureValid() && isHeaderValid() && isNumberOfColorPlanesValid() && isNumberOfBitsPerPixelValid();}
 
 bool AprgBitmap::isSignatureValid() const
 {
@@ -133,97 +137,50 @@ bool AprgBitmap::isCompressedMethodSupported() const
     return (m_compressionMethodType == BitmapCompressedMethodType::BI_RGB);
 }
 
+bool AprgBitmap::isPositionWithinTheBitmap(AprgBitmap::XY const position) const
+{
+    return position.x < m_bitmapWidth && position.y < m_bitmapHeight;
+}
+
 BitmapCompressedMethodType AprgBitmap::getCompressedMethodType() const
 {
     return m_compressionMethodType;
 }
 
+unsigned int AprgBitmap::getMemoryConsumption() const
+{
+    return m_snippetStoredInMemory.pixelData.getSize();
+}
+
 AprgBitmap::XY AprgBitmap::getPositionOfTopLeftCornerInMemory() const
 {
-    return m_snippetStoredInMemory.topLeftCorner;
-}
+    return m_snippetStoredInMemory.topLeftCorner;}
 
 AprgBitmap::XY AprgBitmap::getPositionOfBottomRightCornerInMemory() const
 {
     return m_snippetStoredInMemory.bottomRightCorner;
 }
 
-void AprgBitmap::addPositionToLoadInMemory(AprgBitmap::XY const position)
-{
-    m_snippetStoredInMemory.addPosition(position);
-}
-
-void AprgBitmap::loadPixels()
-{
-    ifstream inputStream(m_path, ios::binary);
-    AlbaFileReader fileReader(inputStream);
-    m_snippetStoredInMemory.pixels.clear();
-
-    int byteOffsetInXForStart = (int)convertPixelsToBytesRoundToFloor(m_snippetStoredInMemory.topLeftCorner.x);
-    int byteOffsetInXForEnd = (int)convertPixelsToBytesRoundToFloor(m_snippetStoredInMemory.bottomRightCorner.x);
-    int offsetInYForStart = m_bitmapHeight-m_snippetStoredInMemory.topLeftCorner.y-1;
-    int offsetInYForEnd = m_bitmapHeight-m_snippetStoredInMemory.bottomRightCorner.y-1;
-    int deltaInX = byteOffsetInXForEnd - byteOffsetInXForStart + 1;
-
-    for(int y=offsetInYForStart; y>=offsetInYForEnd; y--)
-    {
-        unsigned long long fileOffsetForStart = m_pixelArrayAddress+((unsigned long long)m_numberOfBytesPerRow*y)+byteOffsetInXForStart;
-        fileReader.moveLocation(fileOffsetForStart);
-        fileReader.saveDataToMemoryBuffer(m_snippetStoredInMemory.pixels, deltaInX);
-    }
-
-    m_snippetStoredInMemory.topLeftCorner.x = convertBytesToPixels(byteOffsetInXForStart);
-    m_snippetStoredInMemory.topLeftCorner.y = m_snippetStoredInMemory.topLeftCorner.y;
-    m_snippetStoredInMemory.bottomRightCorner.x = convertBytesToPixels(byteOffsetInXForEnd)+getMaximumNumberOfPixelsBeforeOneByte();
-    m_snippetStoredInMemory.bottomRightCorner.y = m_snippetStoredInMemory.bottomRightCorner.y;
-}
-
-void AprgBitmap::clearSnippetInMemory()
-{
-    m_snippetStoredInMemory.clear();
-}
-
-unsigned int AprgBitmap::getPixelAtPositionRestrictedToSnippet(AprgBitmap::XY const position) const
+unsigned int AprgBitmap::getPixelInSnippet(AprgBitmap::XY const position) const
 {
     unsigned int result(0);
-    unsigned int xPartInIndex = convertPixelsToBytesRoundToFloor(position.x)-convertPixelsToBytesRoundToFloor(m_snippetStoredInMemory.topLeftCorner.x);
-    unsigned int oneRowOffset = convertPixelsToBytesRoundToFloor(m_snippetStoredInMemory.bottomRightCorner.x)-convertPixelsToBytesRoundToFloor(m_snippetStoredInMemory.topLeftCorner.x)+1;
-    unsigned int yPartInIndex = oneRowOffset*(position.y-m_snippetStoredInMemory.topLeftCorner.y);
-    unsigned int index = xPartInIndex + yPartInIndex;
-    unsigned char const* reader = (unsigned char const*)m_snippetStoredInMemory.pixels.getConstantBufferPointer();
-    cout<<"postion"<<position.getDisplayableString()<<endl;
-    cout<<"topLeftCorner"<<m_snippetStoredInMemory.topLeftCorner.getDisplayableString()<<endl;
-    cout<<"bottomRightCorner"<<m_snippetStoredInMemory.bottomRightCorner.getDisplayableString()<<endl;
-    cout<<"xPartInIndex"<<xPartInIndex<<endl;
-    cout<<"m_snippetStoredInMemory.bottomRightCorner.x"<<m_snippetStoredInMemory.bottomRightCorner.x<<endl;
-    cout<<"m_snippetStoredInMemory.topLeftCorner.x"<<m_snippetStoredInMemory.topLeftCorner.x<<endl;
-    cout<<"oneRowOffset"<<oneRowOffset<<endl;
-    cout<<"yPartInIndex"<<yPartInIndex<<endl;
-    cout<<"index"<<index<<endl;
-    if(m_numberOfBitsPerPixel <= 8)
+    if(isPositionWithinTheBitmap(position))
     {
-        if(index < m_snippetStoredInMemory.pixels.getSize())
+        unsigned int xPartInIndex = convertPixelsToBytesRoundToFloor(position.x)-convertPixelsToBytesRoundToFloor(m_snippetStoredInMemory.topLeftCorner.x);
+        unsigned int oneRowOffset = convertPixelsToBytesRoundToFloor(m_snippetStoredInMemory.bottomRightCorner.x)-convertPixelsToBytesRoundToFloor(m_snippetStoredInMemory.topLeftCorner.x)+getMinimumNumberOfBytesForOnePixel();
+        unsigned int yPartInIndex = oneRowOffset*(position.y-m_snippetStoredInMemory.topLeftCorner.y);
+        unsigned int index = xPartInIndex + yPartInIndex;
+        unsigned char const* reader = (unsigned char const*)m_snippetStoredInMemory.pixelData.getConstantBufferPointer();
+        if(m_numberOfBitsPerPixel <= 8)
         {
-            result = (unsigned int)(*(reader+index));
-            unsigned int numberOfPixelsInOneByte = convertBytesToPixels(1);
-            unsigned int positionRemainder = position.x%numberOfPixelsInOneByte;
-            unsigned int loopAround = numberOfPixelsInOneByte-positionRemainder-1;
-            unsigned int shiftValue = m_numberOfBitsPerPixel*loopAround;
-            result = (result >> shiftValue) & AlbaBitManipulation<unsigned int>::generateOnesWithNumberOfBits(m_numberOfBitsPerPixel);
+            result = getPixelInSnippetForPixelInAByte(reader, index, position);
+        }
+        else
+        {
+            result = getPixelInSnippetForMultipleBytePixels(reader, index);
         }
     }
     return result;
-}
-
-unsigned int AprgBitmap::getPixelAtPositionWithAutomaticChangesToSnippet(AprgBitmap::XY const position)
-{
-    if(!m_snippetStoredInMemory.isPositionInsideTheSnippet(position))
-    {
-        // this is an inefficient part, think of how to resolve this
-        addPositionToLoadInMemory(position);
-        loadPixels();
-    }
-    return getPixelAtPositionRestrictedToSnippet(position);
 }
 
 unsigned int AprgBitmap::getColorUsingPixelValue(unsigned int pixelValue) const
@@ -252,18 +209,116 @@ AprgBitmap::ColorTable AprgBitmap::getColorTable() const
     return m_colors;
 }
 
-AprgBitmap::Pixels AprgBitmap::getPixelsInMemory() const
+AprgBitmap::PixelData AprgBitmap::getPixelsInMemory() const
 {
-    return m_snippetStoredInMemory.pixels;
+    return m_snippetStoredInMemory.pixelData;
+}
+
+void AprgBitmap::setMaximumMemoryConsumption(unsigned int const maximumMemoryConsumption)
+{
+    m_maximumMemoryConsumption = maximumMemoryConsumption;
+}
+
+unsigned int AprgBitmap::getPixelWithAutomaticChangesToSnippet(AprgBitmap::XY const position)
+{
+    unsigned int result(0);
+    if(isPositionWithinTheBitmap(position))
+    {
+        if(!m_snippetStoredInMemory.isPositionInsideTheSnippet(position))
+        {
+            addPositionInSnippet(position);
+            cout<<"m_snippetStoredInMemory.topLeftCorner"<<m_snippetStoredInMemory.topLeftCorner.getDisplayableString()<<endl;
+            cout<<"m_snippetStoredInMemory.bottomRightCorner"<<m_snippetStoredInMemory.bottomRightCorner.getDisplayableString()<<endl;
+            loadPixels();
+            cout<<"m_snippetStoredInMemory.topLeftCorner"<<m_snippetStoredInMemory.topLeftCorner.getDisplayableString()<<endl;
+            cout<<"m_snippetStoredInMemory.bottomRightCorner"<<m_snippetStoredInMemory.bottomRightCorner.getDisplayableString()<<endl;
+            cout<<"m_bitmapWidth"<<m_bitmapWidth<<endl;
+            cout<<"m_bitmapHeight"<<m_bitmapHeight<<endl;
+        }
+        result = getPixelInSnippet(position);
+    }
+    return result;
+}
+
+void AprgBitmap::addPositionInSnippet(AprgBitmap::XY const position)
+{
+    if(isPositionWithinTheBitmap(position))
+    {
+        m_snippetStoredInMemory.addPosition(position);
+        if(isSnippetBeyondTheMaximumMemoryAllowed())
+        {
+            updateCornersInSnippetForMemoryConsumptionUsage(position);
+        }
+    }
+}
+
+void AprgBitmap::loadPixels()
+{
+    ifstream inputStream(m_path, ios::binary);
+    AlbaFileReader fileReader(inputStream);
+    m_snippetStoredInMemory.pixelData.clear();
+
+    int byteOffsetInXForStart = (int)convertPixelsToBytesRoundToFloor(m_snippetStoredInMemory.topLeftCorner.x);
+    int byteOffsetInXForEnd = (int)convertPixelsToBytesRoundToFloor(m_snippetStoredInMemory.bottomRightCorner.x);
+    int offsetInYForStart = m_bitmapHeight-m_snippetStoredInMemory.topLeftCorner.y-1;
+    int offsetInYForEnd = m_bitmapHeight-m_snippetStoredInMemory.bottomRightCorner.y-1;
+    int numberOfBytesToBeCopiedForX = byteOffsetInXForEnd - byteOffsetInXForStart + getMinimumNumberOfBytesForOnePixel();
+
+    for(int y=offsetInYForStart; y>=offsetInYForEnd; y--)
+    {
+        unsigned long long fileOffsetForStart = m_pixelArrayAddress+((unsigned long long)m_numberOfBytesPerRow*y)+byteOffsetInXForStart;
+        fileReader.moveLocation(fileOffsetForStart);
+        fileReader.saveDataToMemoryBuffer(m_snippetStoredInMemory.pixelData, numberOfBytesToBeCopiedForX);
+    }
+
+    m_snippetStoredInMemory.topLeftCorner.x = convertBytesToPixels(byteOffsetInXForStart);
+    m_snippetStoredInMemory.bottomRightCorner.x = convertBytesToPixels(byteOffsetInXForEnd)+getMaximumNumberOfPixelsBeforeOneByte();
+}
+
+void AprgBitmap::updateCornersInSnippetForMemoryConsumptionUsage(AprgBitmap::XY const center)
+{
+   computeCornersForMaximumMemory(m_snippetStoredInMemory.topLeftCorner, m_snippetStoredInMemory.bottomRightCorner, center);
+}
+
+void AprgBitmap::clearSnippetInMemory(){
+    m_snippetStoredInMemory.clear();
+}
+
+unsigned int AprgBitmap::getPixelInSnippetForPixelInAByte(unsigned char const* reader, unsigned int index, AprgBitmap::XY const position) const
+{
+    unsigned int result(0);
+    if(index < m_snippetStoredInMemory.pixelData.getSize())
+    {
+        result = (unsigned int)(*(reader+index));
+        unsigned int numberOfPixelsInOneByte = convertBytesToPixels(1);
+        unsigned int positionRemainder = position.x%numberOfPixelsInOneByte;
+        unsigned int loopAround = numberOfPixelsInOneByte-positionRemainder-1;
+        unsigned int shiftValue = m_numberOfBitsPerPixel*loopAround;
+        result = (result >> shiftValue) & AlbaBitManipulation<unsigned int>::generateOnesWithNumberOfBits(m_numberOfBitsPerPixel);
+    }
+    return result;
+}
+
+unsigned int AprgBitmap::getPixelInSnippetForMultipleBytePixels(unsigned char const* reader, unsigned int index) const
+{
+    unsigned int result(0);
+    unsigned int minimumNumberOfBytesForOnePixel = getMinimumNumberOfBytesForOnePixel();
+    if(index+minimumNumberOfBytesForOnePixel-1 < m_snippetStoredInMemory.pixelData.getSize())
+    {
+        for(int indexForMultipleBytePixel=minimumNumberOfBytesForOnePixel-1; indexForMultipleBytePixel>=0; indexForMultipleBytePixel--)
+        {
+            result <<= BYTE_SIZE_IN_BITS;
+            result = result | (unsigned int)(*(reader+index+indexForMultipleBytePixel));
+        }
+    }
+    return result;
 }
 
 void AprgBitmap::loadBitmapHeaders()
 {
     //https://en.wikipedia.org/wiki/BMP_file_format
-
     ifstream inputStream(m_path, ios::binary);
     AlbaFileReader fileReader(inputStream);
-
     loadBitmapFileHeader(fileReader);
     loadDibHeader(fileReader);
     loadColorTable(fileReader);
@@ -358,14 +413,80 @@ unsigned int AprgBitmap::getMaximumNumberOfPixelsBeforeOneByte() const
     return (numberOfPixelsInOneByte>0) ? numberOfPixelsInOneByte-1 : 0;
 }
 
+unsigned int AprgBitmap::getMinimumNumberOfBytesForOnePixel() const
+{
+    unsigned int numberOfBytesInOnePixel(convertPixelsToBytesRoundToFloor(1));
+    return (numberOfBytesInOnePixel>0) ? numberOfBytesInOnePixel : 1;
+}
+
+bool AprgBitmap::isSnippetBeyondTheMaximumMemoryAllowed() const
+{
+    unsigned long long possibleSnippetSize = convertPixelsToBytesRoundToCeil(m_snippetStoredInMemory.getNumberOfPixelsInSnippet());
+    return possibleSnippetSize>m_maximumMemoryConsumption || m_snippetStoredInMemory.pixelData.getSize()>m_maximumMemoryConsumption;
+}
+
+AprgBitmap::XY AprgBitmap::computeSquareSideSizeForMaximumMemory() const
+{
+    unsigned int numberOfPixels = (unsigned int)round(convertBytesToPixels(m_maximumMemoryConsumption));
+    unsigned int xSize = convertPixelsToBytesRoundToCeil((unsigned int)sqrt((double)numberOfPixels));
+    unsigned int ySize =  m_maximumMemoryConsumption/xSize;
+    cout<<"numberOfPixels"<<numberOfPixels<<endl;
+    cout<<"xSize"<<xSize<<endl;
+    cout<<"ySize"<<ySize<<endl;
+
+    //think of a better way!!!!//this is your to do
+    return XY(xSize, ySize);
+}
+
+void AprgBitmap::computeCornersForMaximumMemory(AprgBitmap::XY & topLeftCornerPosition, AprgBitmap::XY & bottomRightCornerPosition, AprgBitmap::XY const center) const
+{
+   AprgBitmap::XY rectangularSize(computeSquareSideSizeForMaximumMemory());
+   int sideLeftInX = center.x - (rectangularSize.x/2);
+   int sideTopInY = center.y - (rectangularSize.y/2);
+   int sideRightInX = center.x + (rectangularSize.x/2);
+   int sideBottomInY = center.y + (rectangularSize.y/2);
+   sideLeftInX = (sideLeftInX < 0) ? 0 : sideLeftInX;
+   sideTopInY = (sideTopInY < 0) ? 0 : sideTopInY;
+   sideRightInX = ((unsigned int)sideRightInX >= m_bitmapWidth) ? m_bitmapWidth-1 : sideRightInX;
+   sideBottomInY = ((unsigned int)sideBottomInY >= m_bitmapHeight) ? m_bitmapHeight-1 : sideBottomInY;
+
+   if(sideRightInX-sideLeftInX+1 < (int)rectangularSize.x)
+   {
+       int additionalSizeInX = rectangularSize.x - (sideRightInX-sideLeftInX+1);
+       if((sideLeftInX - additionalSizeInX) >= 0)
+       {
+           sideLeftInX = sideLeftInX - additionalSizeInX;
+       }
+       else if((unsigned int)(sideRightInX + additionalSizeInX) < m_bitmapWidth)
+       {
+           sideRightInX = sideRightInX + additionalSizeInX;
+       }
+   }
+
+   if(sideBottomInY-sideTopInY+1 < (int)rectangularSize.y)
+   {
+       int additionalSizeInY = rectangularSize.y - (sideBottomInY-sideTopInY+1);
+       if((sideTopInY - additionalSizeInY) >= 0)
+       {
+           sideTopInY = sideTopInY - additionalSizeInY;
+       }
+       else if((unsigned int)(sideBottomInY + additionalSizeInY) < m_bitmapHeight)
+       {
+           sideBottomInY = sideBottomInY + additionalSizeInY;
+       }
+   }
+   topLeftCornerPosition.x = (unsigned int) sideLeftInX;
+   topLeftCornerPosition.y = (unsigned int) sideTopInY;
+   bottomRightCornerPosition.x = (unsigned int) sideRightInX;
+   bottomRightCornerPosition.y = (unsigned int) sideBottomInY;
+}
+
 BitmapCompressedMethodType AprgBitmap::determineCompressedMethodType(unsigned int compressedMethodValue) const
 {
-    BitmapCompressedMethodType compressedMethodType;
-    switch(compressedMethodValue)
+    BitmapCompressedMethodType compressedMethodType;    switch(compressedMethodValue)
     {
     case 0: compressedMethodType = BitmapCompressedMethodType::BI_RGB; break;
-    case 1: compressedMethodType = BitmapCompressedMethodType::BI_RLE8; break;
-    case 2: compressedMethodType = BitmapCompressedMethodType::BI_RLE4; break;
+    case 1: compressedMethodType = BitmapCompressedMethodType::BI_RLE8; break;    case 2: compressedMethodType = BitmapCompressedMethodType::BI_RLE4; break;
     case 3: compressedMethodType = BitmapCompressedMethodType::BI_BITFIELDS; break;
     case 4: compressedMethodType = BitmapCompressedMethodType::BI_JPEG; break;
     case 5: compressedMethodType = BitmapCompressedMethodType::BI_PNG; break;
--- AprgBitmap/AprgBitmap/src/AprgBitmap.hpp
+++ AprgBitmap/AprgBitmap/src/AprgBitmap.hpp
@@ -28,78 +28,81 @@ class AprgBitmap
 {
 public:
     static unsigned char const BYTE_SIZE_IN_BITS = 8;
+    static unsigned int const DEFAULT_MAXIMUM_MEMORY_CONSUMPTION = 100000;
     using ColorTable = std::vector<unsigned int>;
-    using Pixels = AlbaMemoryBuffer;
+    using PixelData = AlbaMemoryBuffer;
     struct XY
     {
-        XY();
-        XY(unsigned int xValue, unsigned int yValue);
+        XY();        XY(unsigned int xValue, unsigned int yValue);
         bool operator==(XY const& xy) const;
         std::string getDisplayableString() const;
-        unsigned int x;
-        unsigned int y;
+        unsigned int x;        unsigned int y;
     };
 
 private:
     struct RectangularPixelsSnippet
     {
         RectangularPixelsSnippet();
-        void addPosition(AprgBitmap::XY const position);
+        void addPosition(XY const position);
         void clear();
         unsigned int getDeltaX() const;
         unsigned int getDeltaY() const;
-        bool isPositionInsideTheSnippet(AprgBitmap::XY const position) const;
+        unsigned int getNumberOfPixelsInSnippet() const;
+        void setCorners(XY const topLeftCornerPosition, XY const bottomRightCornerPosition);
+        bool isPositionInsideTheSnippet(XY const position) const;
         bool noPositionIsAddedYet;
-        AprgBitmap::XY topLeftCorner;
-        AprgBitmap::XY bottomRightCorner;
-        AprgBitmap::XY retangularSize;
-        Pixels pixels;
+        XY topLeftCorner;
+        XY bottomRightCorner;
+        XY retangularSize;
+        PixelData pixelData;
     };
 
 public:
-
     AprgBitmap(std::string const& path);
     bool isValid() const;
-    bool isSignatureValid() const;
-    bool isHeaderValid() const;
+    bool isSignatureValid() const;    bool isHeaderValid() const;
     bool isNumberOfColorPlanesValid() const;
     bool isNumberOfBitsPerPixelValid() const;
     bool isCompressedMethodSupported() const;
+    bool isPositionWithinTheBitmap(XY const position) const;
     BitmapCompressedMethodType getCompressedMethodType() const;
+    unsigned int getMemoryConsumption() const;
     XY getPositionOfTopLeftCornerInMemory() const;
     XY getPositionOfBottomRightCornerInMemory()const;
-    void addPositionToLoadInMemory(AprgBitmap::XY const position);
-
-    void loadPixels();
-    void clearSnippetInMemory();
-    unsigned int getPixelAtPositionRestrictedToSnippet(AprgBitmap::XY const position) const;
-    unsigned int getPixelAtPositionWithAutomaticChangesToSnippet(AprgBitmap::XY const position);
+    unsigned int getPixelInSnippet(XY const position) const;
     unsigned int getColorUsingPixelValue(unsigned int pixelValue) const;
-
     ColorTable getColorTable() const;
-    Pixels getPixelsInMemory() const;
+    PixelData getPixelsInMemory() const;
+
+    void setMaximumMemoryConsumption(unsigned int const maximumMemoryConsumption);
+    unsigned int getPixelWithAutomaticChangesToSnippet(XY const position);
+    void addPositionInSnippet(XY const position);
+    void loadPixels();
+    void updateCornersInSnippetForMemoryConsumptionUsage(XY const center);
+    void clearSnippetInMemory();
 
 private:
+    unsigned int getPixelInSnippetForPixelInAByte(unsigned char const* reader, unsigned int index, XY const position) const;
+    unsigned int getPixelInSnippetForMultipleBytePixels(unsigned char const* reader, unsigned int index) const;
     void loadBitmapHeaders();
     void loadBitmapFileHeader(AlbaFileReader& fileReader);
-    void loadDibHeader(AlbaFileReader& fileReader);
-    void loadColorTable(AlbaFileReader& fileReader);
+    void loadDibHeader(AlbaFileReader& fileReader);    void loadColorTable(AlbaFileReader& fileReader);
     void calculateOtherValues();
     unsigned int convertPixelsToBytesRoundToFloor(unsigned int pixels) const;
     unsigned int convertPixelsToBytesRoundToCeil(unsigned int pixels) const;
     unsigned int convertBytesToPixels(unsigned int bytes) const;
     unsigned int getMaximumNumberOfPixelsBeforeOneByte() const;
+    unsigned int getMinimumNumberOfBytesForOnePixel() const;
+    bool isSnippetBeyondTheMaximumMemoryAllowed() const;
+    XY computeSquareSideSizeForMaximumMemory() const;
+    void computeCornersForMaximumMemory(XY & topLeftCornerPosition, XY & bottomRightCornerPosition, XY const center) const;
     BitmapCompressedMethodType determineCompressedMethodType(unsigned int compressedMethodValue) const;
-    std::string m_path;
-    std::string m_signature;
     unsigned int m_fileSize;
     unsigned int m_pixelArrayAddress;
-    unsigned int m_sizeOfHeader;
-    unsigned int m_bitmapWidth;
+    unsigned int m_sizeOfHeader;    unsigned int m_bitmapWidth;
     unsigned int m_bitmapHeight;
     unsigned int m_numberOfColorPlanes;
-    unsigned int m_numberOfBitsPerPixel;
-    BitmapCompressedMethodType m_compressionMethodType;
+    unsigned int m_numberOfBitsPerPixel;    BitmapCompressedMethodType m_compressionMethodType;
     unsigned int m_imageSize;
     unsigned long m_bitmapSize;
     unsigned int m_horizontalResolutionPixelInAMeter;
@@ -109,8 +112,10 @@ private:
     unsigned int m_numberOfBytesForDataInRow;
     unsigned int m_paddingForRowMemoryAlignment;
     unsigned int m_numberOfBytesPerRow;
+    unsigned int m_maximumMemoryConsumption;
+    std::string m_path;
+    std::string m_signature;
     ColorTable m_colors;
     RectangularPixelsSnippet m_snippetStoredInMemory;
 };
-
 }
