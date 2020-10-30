5f90401719cf1baee4e93be9b1900d668da7de07
mevalba <markearvinalba@gmail.com>
2020-10-30 12:36:44 +0800

    Introduce albaxy

---
+++ AprgCommon/AprgCommon/src/Bit/AlbaBitConstants.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace alba
+{
+
+namespace AlbaBitConstants
+{
+    unsigned char const BYTE_SIZE_IN_BITS = 8;
+    unsigned char const BYTE_MASK = 0xFF;
+}
+
+}
---
+++ AprgCommon/AprgCommon/tst/AlbaXY_unit.cpp
@@ -0,0 +1,74 @@
+#include <Container/AlbaXY.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace alba;
+using namespace std;
+
+TEST(AlbaXYTest, DefaultValuesAreZero)
+{
+    AlbaXY<int> xy;
+    EXPECT_EQ(0, xy.getX());
+    EXPECT_EQ(0, xy.getY());
+}
+
+TEST(AlbaXYTest, ValuesAreSetAtConstruction)
+{
+    AlbaXY<int> xy(-5,5);
+    EXPECT_EQ(-5, xy.getX());
+    EXPECT_EQ(5, xy.getY());
+}
+
+TEST(AlbaXYTest,  ValuesCanBeSetAfterConstruction)
+{
+    AlbaXY<int> xy(-5,5);
+    xy.setXAndY(10,10);
+    EXPECT_EQ(AlbaXY<int>(10,10), xy);
+
+    xy.setX(23);
+    xy.setY(45);
+    EXPECT_EQ(AlbaXY<int>(23,45), xy);
+}
+
+TEST(AlbaXYTest, XYCanBeCompared)
+{
+    AlbaXY<int> xy(-5,5);
+    EXPECT_NE(AlbaXY<int>(-5,-5), xy);
+    EXPECT_EQ(AlbaXY<int>(-5,5), xy);
+    EXPECT_NE(AlbaXY<int>(5,-5), xy);
+    EXPECT_NE(AlbaXY<int>(5,5), xy);
+}
+
+TEST(AlbaXYTest, XTimesYCanBeComputed)
+{
+    AlbaXY<int> xy(-5,5);
+    EXPECT_EQ(-25, xy.getXTimesY());
+}
+
+TEST(AlbaXYTest, DisplayableStringIsNotEmpty)
+{
+    AlbaXY<int> xy(-5,5);
+    EXPECT_FALSE(xy.getDisplayableString().empty());
+}
+
+TEST(AlbaXYTest, XYSaveTopLeftCorner)
+{
+    AlbaXY<int> xy;
+    xy.saveAsTopLeftCorner(AlbaXY<int>(-5,5));
+    xy.saveAsTopLeftCorner(AlbaXY<int>(5,-5));
+    xy.saveAsTopLeftCorner(AlbaXY<int>(-5,-5));
+    xy.saveAsTopLeftCorner(AlbaXY<int>(5,5));
+
+    EXPECT_EQ(AlbaXY<int>(-5,-5), xy);
+}
+
+TEST(AlbaXYTest, XYSaveBottomRightCorner)
+{
+    AlbaXY<int> xy;
+    xy.saveAsBottomRightCorner(AlbaXY<int>(-5,5));
+    xy.saveAsBottomRightCorner(AlbaXY<int>(5,-5));
+    xy.saveAsBottomRightCorner(AlbaXY<int>(-5,-5));
+    xy.saveAsBottomRightCorner(AlbaXY<int>(5,5));
+
+    EXPECT_EQ(AlbaXY<int>(5,5), xy);
+}
