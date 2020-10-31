b8c7a324d19b6bc3759e0e92653c009a57d08335
mevalba <markearvinalba@gmail.com>
2020-10-31 11:04:49 +0800

    Introduce soosa project

---
+++ SOOSA/SOOSA/src/SOOSA.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace alba
+{
+
+class SimplestDesignToCopy
+{
+public:
+    SimplestDesignToCopy();
+
+private:
+    int m_sample;
+};
+
+}
---
+++ SOOSA/SOOSA/tst/SOOSA_unit.cpp
@@ -0,0 +1,11 @@
+#include <SimplestDesignToCopy.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace alba;
+using namespace std;
+
+TEST(SampleTest, DISABLED_SampleTest1)
+{
+    SimplestDesignToCopy entity;
+}
