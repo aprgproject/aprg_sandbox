6d20648731ada5c8bba81f192cf2e5e7009a7340
mevalba <markearvinalba@gmail.com>
2020-11-17 16:33:43 +0800

    Enhance macros in aprgcommon

--- AprgCommon/AprgCommon/tst/AlbaMacros_unit.cpp
+++ AprgCommon/AprgCommon/tst/AlbaMacros_unit.cpp
@@ -33,3 +33,42 @@ TEST(AlbaMacros, DisplayMessageTest)
     EXPECT_EQ("ALBA_MACROS_DISPLAY_VALUE_PRAGMA_MESSAGE_SAMPLE_MACRO=10000", ALBA_MACROS_DISPLAY_VALUE_PRAGMA_MESSAGE(ALBA_MACROS_DISPLAY_VALUE_PRAGMA_MESSAGE_SAMPLE_MACRO));
     #pragma message(ALBA_MACROS_DISPLAY_VALUE_PRAGMA_MESSAGE(ALBA_MACROS_DISPLAY_VALUE_PRAGMA_MESSAGE_SAMPLE_MACRO))
 }
+
+TEST(AlbaMacros, GetStringLiteral)
+{
+    enum class SampleEnumClass
+    {
+        Type1,
+        Type2
+    };
+
+    EXPECT_EQ("12345", ALBA_MACROS_GET_STRING_LITERAL(12345));
+    EXPECT_EQ("SampleEnumClass::Type1", ALBA_MACROS_GET_STRING_LITERAL(SampleEnumClass::Type1));
+    EXPECT_EQ("SampleEnumClass::Type2", ALBA_MACROS_GET_STRING_LITERAL(SampleEnumClass::Type2));
+}
+
+TEST(AlbaMacros, CaseEnumStringLiteral)
+{
+    enum class SampleEnumClass
+    {
+        Type1,
+        Type2
+    };
+    struct SampleClass
+    {
+        string getString(SampleEnumClass const enumValue)
+        {
+            switch(enumValue)
+            {
+            ALBA_MACROS_CASE_ENUM_STRING(SampleEnumClass::Type1)
+                    ALBA_MACROS_CASE_ENUM_STRING(SampleEnumClass::Type2)
+            }
+        }
+    };
+    SampleEnumClass enumValue1 = SampleEnumClass::Type1;
+    SampleEnumClass enumValue2 = SampleEnumClass::Type2;
+
+    SampleClass sampleObject;
+    EXPECT_EQ("SampleEnumClass::Type1", sampleObject.getString(enumValue1));
+    EXPECT_EQ("SampleEnumClass::Type2", sampleObject.getString(enumValue2));
+}
