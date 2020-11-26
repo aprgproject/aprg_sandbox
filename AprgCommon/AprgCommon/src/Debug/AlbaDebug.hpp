e8ef48fdbaf63bee3f467dc40d8ee034d14e8123
mevalba <markearvinalba@gmail.com>
2020-11-26 18:37:25 +0800

    Refactor albadebug module

--- AprgCommon/AprgCommon/src/Debug/AlbaDebug.hpp
+++ AprgCommon/AprgCommon/src/Debug/AlbaDebug.hpp
@@ -2,39 +2,35 @@
 
 #include <Macros/AlbaMacros.hpp>
 
-#include <windows.h>
-
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-namespace alba
-{
+namespace alba{
 
 #pragma warning("The class AlbaDebug is used. Please remove after your done debugging.")
 
-#define ALBA_DEBUG_GET_STRING_LITERALS_EXPANSION(parameter1, parameter2) ALBA_MACROS_GET_STRING_LITERAL(parameter1) + parameter2
-#define ALBA_DEBUG_GET_STRING_LITERALS1(parameter1)                      ALBA_MACROS_GET_STRING_LITERAL(parameter1)
-#define ALBA_DEBUG_GET_STRING_LITERALS2(parameter1, parameter2)          ALBA_MACROS_GET_STRING_LITERAL(parameter1) + ALBA_MACROS_GET_STRING_LITERAL(parameter2)
-#define ALBA_DEBUG_GET_STRING_LITERALS3(parameter, ...)                  ALBA_DEBUG_GET_STRING_LITERALS_EXPANSION(parameter, ALBA_DEBUG_GET_STRING_LITERALS2(__VA_ARGS__))
-#define ALBA_DEBUG_GET_STRING_LITERALS4(parameter, ...)                  ALBA_DEBUG_GET_STRING_LITERALS_EXPANSION(parameter, ALBA_DEBUG_GET_STRING_LITERALS2(__VA_ARGS__))
-#define ALBA_DEBUG_GET_STRING_LITERALS5(parameter, ...)                  ALBA_DEBUG_GET_STRING_LITERALS_EXPANSION(parameter, ALBA_DEBUG_GET_STRING_LITERALS2(__VA_ARGS__))
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS_EXPANSION(parameter1, parameter2) ALBA_MACROS_GET_STRING_LITERAL(parameter1) + parameter2
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS1(parameter1)                      ALBA_MACROS_GET_STRING_LITERAL(parameter1)
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS2(parameter1, parameter2)          ALBA_MACROS_GET_STRING_LITERAL(parameter1) + ALBA_MACROS_GET_STRING_LITERAL(parameter2)
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS3(parameter, ...)                  Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS_EXPANSION(parameter, Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS2(__VA_ARGS__))
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS4(parameter, ...)                  Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS_EXPANSION(parameter, Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS2(__VA_ARGS__))
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS5(parameter, ...)                  Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS_EXPANSION(parameter, Z_ALBA_PRIVATE_DEBUG_GET_STRING_LITERALS2(__VA_ARGS__))
 
-#define ALBA_DEBUG_GET_STRING_PRINT_EXPANSION(parameter1, parameter2) parameter1 << parameter2
-#define ALBA_DEBUG_GET_STRING_PRINT1(parameter1)                      ALBA_MACROS_GET_STRING_LITERAL(parameter1) << ":[" << parameter1 << "] "
-#define ALBA_DEBUG_GET_STRING_PRINT2(parameter1, parameter2)          ALBA_MACROS_GET_STRING_LITERAL(parameter1) << ":[" << parameter1 << "] " << ALBA_MACROS_GET_STRING_LITERAL(parameter2) << ":[" << parameter2 << "] "
-#define ALBA_DEBUG_GET_STRING_PRINT3(parameter, ...)                  ALBA_DEBUG_GET_STRING_PRINT_EXPANSION(ALBA_DEBUG_GET_STRING_PRINT1(parameter), ALBA_DEBUG_GET_STRING_PRINT2(__VA_ARGS__))
-#define ALBA_DEBUG_GET_STRING_PRINT4(parameter, ...)                  ALBA_DEBUG_GET_STRING_PRINT_EXPANSION(ALBA_DEBUG_GET_STRING_PRINT1(parameter), ALBA_DEBUG_GET_STRING_PRINT3(__VA_ARGS__))
-#define ALBA_DEBUG_GET_STRING_PRINT5(parameter, ...)                  ALBA_DEBUG_GET_STRING_PRINT_EXPANSION(ALBA_DEBUG_GET_STRING_PRINT1(parameter), ALBA_DEBUG_GET_STRING_PRINT4(__VA_ARGS__))
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT_EXPANSION(parameter1, parameter2) parameter1 << parameter2
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT1(parameter1)                      ALBA_MACROS_GET_STRING_LITERAL(parameter1) << ":[" << parameter1 << "] "
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT2(parameter1, parameter2)          ALBA_MACROS_GET_STRING_LITERAL(parameter1) << ":[" << parameter1 << "] " << ALBA_MACROS_GET_STRING_LITERAL(parameter2) << ":[" << parameter2 << "] "
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT3(parameter, ...)                  Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT_EXPANSION(Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT1(parameter), Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT2(__VA_ARGS__))
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT4(parameter, ...)                  Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT_EXPANSION(Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT1(parameter), Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT3(__VA_ARGS__))
+#define Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT5(parameter, ...)                  Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT_EXPANSION(Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT1(parameter), Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT4(__VA_ARGS__))
 
-#define ALBA_DEBUG_PAUSE system("pause")
+#define ALBA_DEBUG_EXIT exit(0)
 #define ALBA_DEBUG_PRINT0(parameter) cout << "ALBA_DEBUG_PRINT0:" << parameter << endl
-#define ALBA_DEBUG_PRINT1(...) cout << "ALBA_DEBUG_PRINT1:" << ALBA_DEBUG_GET_STRING_PRINT1(__VA_ARGS__) << endl
-#define ALBA_DEBUG_PRINT2(...) cout << "ALBA_DEBUG_PRINT2:" << ALBA_DEBUG_GET_STRING_PRINT2(__VA_ARGS__) << endl
-#define ALBA_DEBUG_PRINT3(...) cout << "ALBA_DEBUG_PRINT3:" << ALBA_DEBUG_GET_STRING_PRINT3(__VA_ARGS__) << endl
-#define ALBA_DEBUG_PRINT4(...) cout << "ALBA_DEBUG_PRINT4:" << ALBA_DEBUG_GET_STRING_PRINT4(__VA_ARGS__) << endl
-#define ALBA_DEBUG_PRINT5(...) cout << "ALBA_DEBUG_PRINT5:" << ALBA_DEBUG_GET_STRING_PRINT5(__VA_ARGS__) << endl
+#define ALBA_DEBUG_PRINT1(...) cout << "ALBA_DEBUG_PRINT1:" << Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT1(__VA_ARGS__) << endl
+#define ALBA_DEBUG_PRINT2(...) cout << "ALBA_DEBUG_PRINT2:" << Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT2(__VA_ARGS__) << endl
+#define ALBA_DEBUG_PRINT3(...) cout << "ALBA_DEBUG_PRINT3:" << Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT3(__VA_ARGS__) << endl
+#define ALBA_DEBUG_PRINT4(...) cout << "ALBA_DEBUG_PRINT4:" << Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT4(__VA_ARGS__) << endl
+#define ALBA_DEBUG_PRINT5(...) cout << "ALBA_DEBUG_PRINT5:" << Z_ALBA_PRIVATE_DEBUG_GET_STRING_PRINT5(__VA_ARGS__) << endl
 
 
 }//namespace alba
