95659d18fffb4e05926bc8ef4ef629a78b33202d
mevalba <markearvinalba@gmail.com>
2020-12-08 21:17:43 +0800

    Implemented binary search functionality in APRGAlgorithm.

--- AprgAlgorithm/AprgAlgorithm/cmake/FilesAndDirectories.cmake
+++ AprgAlgorithm/AprgAlgorithm/cmake/FilesAndDirectories.cmake
@@ -7,14 +7,13 @@ set(APRG_ALGORITHM_TEST_INCLUDE_DIRECTORIES
 )
 
 set(APRG_ALGORITHM_SOURCES
+    ${APRG_ALGORITHM_DIR}/src/BinarySearch/BinarySearch.hpp
     ${APRG_ALGORITHM_DIR}/src/GrepStringEvaluator/AlbaGrepStringEvaluator.cpp
     ${APRG_ALGORITHM_DIR}/src/GrepStringEvaluator/AlbaGrepStringEvaluator.hpp
-    ${APRG_ALGORITHM_DIR}/src/GrepStringEvaluator/AlbaGrepStringEvaluatorTerm.cpp
-    ${APRG_ALGORITHM_DIR}/src/GrepStringEvaluator/AlbaGrepStringEvaluatorTerm.hpp
+    ${APRG_ALGORITHM_DIR}/src/GrepStringEvaluator/AlbaGrepStringEvaluatorTerm.cpp    ${APRG_ALGORITHM_DIR}/src/GrepStringEvaluator/AlbaGrepStringEvaluatorTerm.hpp
     ${APRG_ALGORITHM_DIR}/src/LargeSorter/AlbaLargeSorter.hpp
     ${APRG_ALGORITHM_DIR}/src/LargeSorter/DataBlockCache.hpp
-    ${APRG_ALGORITHM_DIR}/src/LargeSorter/AlbaLargeSorterConfiguration.hpp
-    ${APRG_ALGORITHM_DIR}/src/LargeSorter/AlbaLargeSorterTypes.hpp
+    ${APRG_ALGORITHM_DIR}/src/LargeSorter/AlbaLargeSorterConfiguration.hpp    ${APRG_ALGORITHM_DIR}/src/LargeSorter/AlbaLargeSorterTypes.hpp
     ${APRG_ALGORITHM_DIR}/src/LargeSorter/DataBlock.hpp
     ${APRG_ALGORITHM_DIR}/src/LargeSorter/DataBlockFileHandler.hpp
     ${APRG_ALGORITHM_DIR}/src/LargeSorter/DataBlockMemoryHandler.hpp
@@ -28,11 +27,10 @@ set(APRG_ALGORITHM_TESTS
     ${APRG_ALGORITHM_DIR}/tst/AlbaGrepStringEvaluator_unit.cpp
     ${APRG_ALGORITHM_DIR}/tst/AlbaLargeSorter_unit.cpp
     ${APRG_ALGORITHM_DIR}/tst/AlbaLargeSorterProfiling_unit.cpp
+    ${APRG_ALGORITHM_DIR}/tst/BinarySearch_unit.cpp
     ${APRG_ALGORITHM_DIR}/tst/QuineMcCluskey_unit.cpp
     ${APRG_ALGORITHM_DIR}/tst/UnionFind_unit.cpp
 )
-
 set(APRG_ALGORITHM_SOURCES_AND_TESTS
     ${APRG_ALGORITHM_SOURCES}
-    ${APRG_ALGORITHM_TESTS}
-)
+    ${APRG_ALGORITHM_TESTS})
