58f9230c55bbecb02aa7902034612c53ada49087
mevalba <markearvinalba@gmail.com>
2021-01-30 13:04:27 +0800

    Continued refactoring in SimpleStdDesignToCopy module.

--- SimplestDesignToCopyForLibraries/AprgCmakeInclude/changename.cmake
+++
@@ -1,8 +0,0 @@
-#Assign AprgCommon directory, this needs aprg directory
-set(SIMPLEST_DESIGN_TO_COPY_DIR ${APRG_DIR}/AprgAlgorithm/AprgAlgorithm/)
-
-#Include APRG_COMMON files and directories
-include(${SIMPLEST_DESIGN_TO_COPY_DIR}/cmake/FilesAndDirectories.cmake)
-
-include_directories(${SIMPLEST_DESIGN_TO_COPY_INCLUDE_DIRECTORIES})
-add_library(SIMPLEST_DESIGN_TO_COPY ${SIMPLEST_DESIGN_TO_COPY_SOURCES})
