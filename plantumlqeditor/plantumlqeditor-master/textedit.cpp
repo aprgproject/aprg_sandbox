e4ee57b05e70e9748b0fc29943f57afd5ddeeaa7
mevalba <markearvinalba@gmail.com>
2021-01-31 20:45:50 +0800

    Resolved PlantUML-related issues.

--- plantumlqeditor/plantumlqeditor-master/textedit.cpp
+++ plantumlqeditor/plantumlqeditor-master/textedit.cpp
@@ -99,15 +99,16 @@ void TextEdit::keyPressEvent(QKeyEvent *e)
         case Qt::Key_Return:
         case Qt::Key_Enter:
         {
-            QPlainTextEdit::keyPressEvent(e);
+            QString nextLine;
+            nextLine = QChar('\n');
+            textCursor().insertText(nextLine);
+            //QPlainTextEdit::keyPressEvent(e);
             QTextCursor updateCursor = textCursor();
 
-            // Auto-indent
-            if (_autoIndent)
+            // Auto-indent            if (_autoIndent)
             {
                 QTextBlock block          = updateCursor.block().previous();
-                QString    data           = block.text();
-                int        pos            = block.length();
+                QString    data           = block.text();                int        pos            = block.length();
 
                 int i;
                 if (pos >= data.length())
