d7f96e7f2f027d9f445e69dd9831664309f30cc5
mevalba <markearvinalba@gmail.com>
2021-02-06 09:56:50 +0800

    Improved SackReader functionalities.

--- AprgCommon/AprgCommon/src/File/AlbaFileParameterReader.hpp
+++ AprgCommon/AprgCommon/src/File/AlbaFileParameterReader.hpp
@@ -2,9 +2,9 @@
 
 #include <fstream>
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
-
 namespace alba
 {
 
@@ -14,10 +14,10 @@ public:
     explicit AlbaFileParameterReader(std::istream& stream);
     template<typename TypeToRetrieve> TypeToRetrieve readData() const;
     template<typename TypeToRetrieve> void readData(std::vector<TypeToRetrieve> & vectorOfData) const;
+    template<typename TypeToRetrieve> void readData(std::set<TypeToRetrieve> & setOfData) const;
     template<typename TypeToRetrieve1, typename TypeToRetrieve2> void readData(std::map<TypeToRetrieve1, TypeToRetrieve2> & mapOfData) const
 ;
-private:
-    std::istream& m_stream;
+private:    std::istream& m_stream;
 };
 
 template<typename TypeToRetrieve>
@@ -42,10 +42,20 @@ void AlbaFileParameterReader::readData(std::vector<TypeToRetrieve> & vectorOfDat
     }
 }
 
-template<typename TypeToRetrieve1, typename TypeToRetrieve2>
-void AlbaFileParameterReader::readData(std::map<TypeToRetrieve1, TypeToRetrieve2> & mapOfData) const
+template<typename TypeToRetrieve>
+void AlbaFileParameterReader::readData(std::set<TypeToRetrieve> & setOfData) const //test
 {
     unsigned int size;
+    m_stream >> size;
+    for(unsigned int i=0; i<size; i++)
+    {
+        setOfData.emplace(readData<TypeToRetrieve>());
+    }
+}
+
+template<typename TypeToRetrieve1, typename TypeToRetrieve2>
+void AlbaFileParameterReader::readData(std::map<TypeToRetrieve1, TypeToRetrieve2> & mapOfData) const
+{    unsigned int size;
     m_stream >> size;
     for(unsigned int i=0; i<size; i++)
     {
--- AprgCommon/AprgCommon/src/File/AlbaFileParameterWriter.hpp
+++ AprgCommon/AprgCommon/src/File/AlbaFileParameterWriter.hpp
@@ -2,9 +2,9 @@
 
 #include <fstream>
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
-
 namespace alba
 {
 
@@ -14,10 +14,10 @@ public:
     explicit AlbaFileParameterWriter(std::ostream& stream);
     template<typename TypeToWrite> void writeData(TypeToWrite const& data) const;
     template<typename TypeToWrite> void writeData(std::vector<TypeToWrite> const& vectorOfData) const;
+    template<typename TypeToWrite> void writeData(std::set<TypeToWrite> const& setOfData) const;
     template<typename TypeToWrite1, typename TypeToWrite2> void writeData(std::map<TypeToWrite1, TypeToWrite2> const& mapOfData) const;
 
-private:
-    std::ostream& m_stream;
+private:    std::ostream& m_stream;
 };
 
 template<typename TypeToRetrieve>
@@ -39,10 +39,19 @@ void AlbaFileParameterWriter::writeData(std::vector<TypeToWrite> const& vectorOf
     }
 }
 
+template<typename TypeToWrite>
+void AlbaFileParameterWriter::writeData(std::set<TypeToWrite> const& setOfData) const //test
+{
+    m_stream << setOfData.size() << std::endl;
+    for(TypeToWrite const& data : setOfData)
+    {
+        writeData(data);
+    }
+}
+
 template<typename TypeToWrite1, typename TypeToWrite2>
 void AlbaFileParameterWriter::writeData(std::map<TypeToWrite1, TypeToWrite2> const& mapOfData) const
-{
-    m_stream << mapOfData.size() << std::endl;
+{    m_stream << mapOfData.size() << std::endl;
     for(std::pair<TypeToWrite1, TypeToWrite2> const& dataPair : mapOfData)
     {
         writeData<TypeToWrite1>(dataPair.first);
