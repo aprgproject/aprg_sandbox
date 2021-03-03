295b3fb4ba2c03a2d4291ca64a59f33a14a7a718
mevalba <markearvinalba@gmail.com>
2021-03-03 22:05:25 +0800

    Integrated AlbaSingleton in APRGCommon.

---
+++ AprgCommon/AprgCommon/src/Container/AlbaSingleton.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace alba
+{
+
+template<class ObjectType>
+class AlbaSingleton
+{
+public:
+    static ObjectType & getInstance();
+
+protected:
+    AlbaSingleton() {}
+
+private:
+    AlbaSingleton(AlbaSingleton const&) = delete;
+    AlbaSingleton & operator = (AlbaSingleton const&) = delete;
+};
+
+template<class ObjectType>
+ObjectType & AlbaSingleton<ObjectType>::getInstance()
+{
+    static ObjectType instance;
+    return instance;
+}
+
+} // namespace alba
