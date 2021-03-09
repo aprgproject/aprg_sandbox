98d605f9b789d11ddd7cffa4ce56f1a5c08697bb
mevalba <markearvinalba@gmail.com>
2021-03-09 12:04:21 +0800

    Conducted refactoring in APRGGraph.

--- AprgGraph/AprgCmakeInclude/AprgGraph.cmake
+++ AprgGraph/AprgCmakeInclude/AprgGraph.cmake
@@ -5,4 +5,4 @@ set(APRG_GRAPH_DIR ${APRG_DIR}/AprgAlgorithm/AprgAlgorithm/)
 include(${APRG_GRAPH_DIR}/cmake/FilesAndDirectories.cmake)
 
 include_directories(${APRG_GRAPH_INCLUDE_DIRECTORIES})
-add_library(SIMPLEST_DESIGN_TO_COPY ${APRG_GRAPH_SOURCES})
+add_library(APRG_GRAPH ${APRG_GRAPH_SOURCES})
