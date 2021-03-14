ef295ac500aa833d3afc01ba647c99c7aa626607
mevalba <markearvinalba@gmail.com>
2021-03-14 11:06:30 +0800

    Implemented PIDSimulator's main functionality.

--- PidSimulator/PidSimulator/main/main.cpp
+++ PidSimulator/PidSimulator/main/main.cpp
@@ -11,20 +11,18 @@ int main(int argc, char * argv[])
     stringHelper::strings argumentsInMain;
     stringHelper::fetchArgumentsToStringInMain(argumentsInMain, argc, argv);
 
-    argumentsInMain.emplace_back("kp=0.1");
+    /*argumentsInMain.emplace_back("kp=0.1");
     argumentsInMain.emplace_back("ki=0.2");
     argumentsInMain.emplace_back("kd=0.02");
-    argumentsInMain.emplace_back("numberOfLoopsOfPeriodicInputDemand=1");
-    argumentsInMain.emplace_back("amplitudeOfInputDemand=10");
+    argumentsInMain.emplace_back("numberOfLoopsOfPeriodicInputDemand=1");    argumentsInMain.emplace_back("amplitudeOfInputDemand=10");
     argumentsInMain.emplace_back("numberOfSamplesOfInputDemandInOnePeriod=100");
     argumentsInMain.emplace_back("addedOffsetOfInputDemand=5");
     argumentsInMain.emplace_back("targetInPidCalculation=6");
     argumentsInMain.emplace_back("maxCellTxPower=30");
     argumentsInMain.emplace_back("inputType=sine");
-    argumentsInMain.emplace_back("machsModelType=MachsModel1");
+    argumentsInMain.emplace_back("machsModelType=MachsModel1");*/
 
     PidSimulator simulator(argumentsInMain);
     simulator.generateInput();
-
     simulator.calculateAndGenerateOutputImage();
 }
