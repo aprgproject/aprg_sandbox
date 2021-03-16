40b5bd48f8d1974b8cb7f1d269084ce23240c49c
mevalba <markearvinalba@gmail.com>
2021-03-16 15:45:45 +0800

    Added functionalities to RTT Analyzer.

--- RttAnalyzer/RttAnalyzer/tst/RttAnalyzer_unit.cpp
+++ RttAnalyzer/RttAnalyzer/tst/RttAnalyzer_unit.cpp
@@ -5,7 +5,16 @@
 using namespace alba;
 using namespace std;
 
-TEST(SampleTest, DISABLED_SampleTest1)
+TEST(SampleTest, SampleTest1)
 {
-    RttAnalyzer entity;
+    RttAnalyzer rttAnalyzer;
+    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssDisabled\M20190315_093739_pos.txt)");
+    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\2f2_M20190315_085317_pos.txt)");
+    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\012_M20190315_080318_pos.txt)");
+    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\022_M20190315_081240_pos.txt)");
+    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\042_M20190315_082008_pos.txt)");
+    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\082_M20190315_082659_pos.txt)");
+    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\102_M20190315_083235_pos.txt)");
+    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\202_M20190315_084457_pos.txt)");
+
 }
