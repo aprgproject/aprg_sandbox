66f5fdbb2447462a7a82a1cc476a7c3e8385d8e9
mevalba <markearvinalba@gmail.com>
2021-03-16 16:19:24 +0800

    Extended functionalities of RTT Analyzer.

--- RttAnalyzer/RttAnalyzer/src/RttAnalyzer.cpp
+++ RttAnalyzer/RttAnalyzer/src/RttAnalyzer.cpp
@@ -41,14 +41,42 @@ void RttAnalyzer::processFile(std::string const& file)
 void RttAnalyzer::processLine(std::string const& line)
 {
     static string dateTime;
-    if(isStringFoundInsideTheOtherStringNotCaseSensitive(line, "0x4179"))
+    if(isStringFoundInsideTheOtherStringNotCaseSensitive(line, "0x4178"))
     {
         dateTime = getStringWithoutStartingAndTrailingWhiteSpace(getStringBeforeThisString(line,"["));
     }
-    else if(isStringFoundInsideTheOtherStringNotCaseSensitive(line, "(Cx8)") && isStringFoundInsideTheOtherStringNotCaseSensitive(line, "PN Pos"))
+    else if(isStringFoundInsideTheOtherStringNotCaseSensitive(line, "PSC") && isStringFoundInsideTheOtherStringNotCaseSensitive(line, "(cx8)"))
     {
+        ALBA_PRINT2("cx8", line);
         strings titles;
         splitToStrings<SplitStringType::WithoutDelimeters>(titles, line, "|");
+        processTitles(titles);
+    }
+    else if(isStringFoundInsideTheOtherStringNotCaseSensitive(line, "SET") && isStringFoundInsideTheOtherStringNotCaseSensitive(line, "|"))
+    {
+        ALBA_PRINT2("|", line);
+        strings values;
+        splitToStrings<SplitStringType::WithoutDelimeters>(values, line, "|");
+        processValues(dateTime, values);
+    }
+    else if(!isStringFoundInsideTheOtherStringNotCaseSensitive(line, "------"))
+    {
+        ALBA_PRINT2("------", line);
+        m_cx8IndexOptional.clear();
+        m_pnPosIndexOptional.clear();
+    }
+}
+
+/*
+void RttAnalyzer::processLine(std::string const& line)
+{
+    static string dateTime;
+    if(isStringFoundInsideTheOtherStringNotCaseSensitive(line, "0x4179"))
+    {
+        dateTime = getStringWithoutStartingAndTrailingWhiteSpace(getStringBeforeThisString(line,"["));    }
+    else if(isStringFoundInsideTheOtherStringNotCaseSensitive(line, "(Cx8)") && isStringFoundInsideTheOtherStringNotCaseSensitive(line, "PN Pos"))
+    {
+        strings titles;        splitToStrings<SplitStringType::WithoutDelimeters>(titles, line, "|");
         processTitles(titles);
     }
     else if(isStringFoundInsideTheOtherStringNotCaseSensitive(line, "|"))
@@ -67,40 +95,67 @@ void RttAnalyzer::processLine(std::string const& line)
         m_pnPosIndexOptional.clear();
     }
 }
+*/
 
 void RttAnalyzer::processTitles(strings const& titles)
-{
-    unsigned int index=0;
+{    unsigned int index=0;
     for(string const& title : titles)
+    {
+        if(isStringFoundInsideTheOtherStringNotCaseSensitive(title, "(cx8)") && !m_cx8IndexOptional.hasContent())
+        {
+            ALBA_PRINT2("processTitles", index);
+            m_cx8IndexOptional.setValue(index);
+            break;
+        }
+        index++;
+    }
+}
+
+/*
+void RttAnalyzer::processTitles(strings const& titles)
+{
+    unsigned int index=0;    for(string const& title : titles)
     {
         if(isStringFoundInsideTheOtherStringNotCaseSensitive(title, "(Cx8)") && !m_cx8IndexOptional.hasContent())
         {
-            m_cx8IndexOptional.setValue(index);
-        }
+            m_cx8IndexOptional.setValue(index);        }
         else if(isStringFoundInsideTheOtherStringNotCaseSensitive(title, "PN Pos") && !m_pnPosIndexOptional.hasContent())
         {
             m_pnPosIndexOptional.setValue(index);
         }
         index++;
     }
-    /*ALBA_PRINT3(index, m_cx8IndexOptional.getReference(), m_pnPosIndexOptional.getReference());
-    index=0;
-    for(string const& title : titles)
-    {
-        ALBA_PRINT2(index, title);
-        index++;
-    }*/
 }
-
+*/
 
 void RttAnalyzer::processValues(string const& dateTime, strings const& values)
 {
     static RttDetails rttDetails;
-    if(m_cx8IndexOptional.hasContent() && m_pnPosIndexOptional.hasContent())
+    ALBA_PRINT2("processValues", dateTime);
+    if(m_cx8IndexOptional.hasContent())
+    {
+        ALBA_PRINT3("m_cx8IndexOptional", m_cx8IndexOptional.getReference(), values.size());
+        if(m_cx8IndexOptional.getReference()<values.size())
+        {
+            unsigned int value = convertStringToNumber<unsigned int>(values[m_cx8IndexOptional.getReference()]);
+            ALBA_PRINT2("value", value);
+            if(value!=0)
+            {
+                rttDetails.multiplePos[0] = value;
+                rttDetails.dateTime = dateTime;
+                m_allRttDetails.emplace_back(rttDetails);
+            }
+        }
+    }
+}
+
+/*
+void RttAnalyzer::processValues(string const& dateTime, strings const& values)
+{
+    static RttDetails rttDetails;    if(m_cx8IndexOptional.hasContent() && m_pnPosIndexOptional.hasContent())
     {
         if(m_cx8IndexOptional.getReference()<values.size() && m_pnPosIndexOptional.getReference()<values.size() && m_posNumber<6)
-        {
-            rttDetails.multiplePos[m_posNumber] = convertStringToNumber<unsigned int>(values[m_cx8IndexOptional.getReference()]);
+        {            rttDetails.multiplePos[m_posNumber] = convertStringToNumber<unsigned int>(values[m_cx8IndexOptional.getReference()]);
             if(m_posNumber==0)
             {
                 rttDetails.pnPos = convertStringToNumber<unsigned int>(values[m_pnPosIndexOptional.getReference()]);
@@ -115,22 +170,26 @@ void RttAnalyzer::processValues(string const& dateTime, strings const& values)
         m_allRttDetails.emplace_back(rttDetails);
         m_posNumber=0;
     }
-    /*unsigned int index=0;
-    for(string const& value : values)
-    {
-        ALBA_PRINT2(index, value);
-        index++;
-    }*/
 }
+*/
 
 void RttAnalyzer::saveAllRttDetails(ofstream & collectedRttDetails) const
 {
-    collectedRttDetails << "dateTime,pos1,pos2,pos3,pos4,pos5,pos6,pn pos,diffPos1,diffPos2,diffPos3,diffPos4,diffPos5,diffPos6,minDifferencePos,maxDifferencePos"<<endl;
+    collectedRttDetails << "dateTime,Position(cx8)"<<endl;
 
     for(RttDetails const& rttDetails : m_allRttDetails)
     {
-        collectedRttDetails << rttDetails.dateTime << ", ";
-        unsigned int minPos(rttDetails.multiplePos[0]), maxPos(rttDetails.multiplePos[0]), totalPos(0);
+        collectedRttDetails << rttDetails.dateTime << ", " << rttDetails.multiplePos[0] << endl;
+    }
+}
+
+/*
+void RttAnalyzer::saveAllRttDetails(ofstream & collectedRttDetails) const
+{
+    collectedRttDetails << "dateTime,pos1,pos2,pos3,pos4,pos5,pos6,pn pos,diffPos1,diffPos2,diffPos3,diffPos4,diffPos5,diffPos6,minDifferencePos,maxDifferencePos"<<endl;
+    for(RttDetails const& rttDetails : m_allRttDetails)
+    {
+        collectedRttDetails << rttDetails.dateTime << ", ";        unsigned int minPos(rttDetails.multiplePos[0]), maxPos(rttDetails.multiplePos[0]), totalPos(0);
         int differentWithPos[6];
         int minDifferencePos, maxDifferencePos;
         for(unsigned int index=0; index<6; index++)
@@ -157,6 +216,6 @@ void RttAnalyzer::saveAllRttDetails(ofstream & collectedRttDetails) const
         collectedRttDetails << endl;
     }
 }
-
+*/
 
 }
--- RttAnalyzer/RttAnalyzer/tst/RttAnalyzer_unit.cpp
+++ RttAnalyzer/RttAnalyzer/tst/RttAnalyzer_unit.cpp
@@ -8,13 +8,8 @@ using namespace std;
 TEST(SampleTest, SampleTest1)
 {
     RttAnalyzer rttAnalyzer;
-    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssDisabled\M20190315_093739_pos.txt)");
-    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\2f2_M20190315_085317_pos.txt)");
-    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\012_M20190315_080318_pos.txt)");
-    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\022_M20190315_081240_pos.txt)");
-    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\042_M20190315_082008_pos.txt)");
-    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\082_M20190315_082659_pos.txt)");
-    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\102_M20190315_083235_pos.txt)");
-    rttAnalyzer.processFile(R"(G:\QCATLOGS\PR418065\UeLogsDssEnabled\202_M20190315_084457_pos.txt)");
+    //rttAnalyzer.processFile(R"(C:\ZZZ_Logs\PR418065\QC_8150_incorrect_RTT\0x002_M20190325_121622_4178.txt)");
+    rttAnalyzer.processFile(R"(C:\ZZZ_Logs\PR418065\QC_8150_incorrect_RTT\0x042_M20190325_122214_4178.txt)");
+
 
 }
