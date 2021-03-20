5c6929131be6ef5b9a5898462a8484cc71430684
mevalba <markearvinalba@gmail.com>
2021-03-21 01:43:37 +0800

    Continued refactoring across APRGCommon and other projects.

---
+++ AprgLocalUserAutomation/AprgLocalUserAutomation/tst/AlbaLocalUserAutomation_unit.cpp
@@ -0,0 +1,12 @@
+#include <AlbaLocalUserAutomation.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace alba;
+using namespace std;
+
+TEST(AlbaLocalUserAutomation, DISABLED_SampleTest1)
+{
+    AlbaLocalUserAutomation entity;
+    // its better to test non OS specific behavior here
+}
