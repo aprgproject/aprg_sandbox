a855692fd5cee22cfdeb1b0d67825406d9ac01d2
mevalba <markearvinalba@gmail.com>
2021-03-30 07:56:04 +0800

    Extended functionalities of GnuplotIostream.

--- GnuplotIostream/GnuplotIostream/tst/AprgGeometryThreeDimensions_unit.cpp
+++ GnuplotIostream/GnuplotIostream/tst/AprgGeometryThreeDimensions_unit.cpp
@@ -8,50 +8,91 @@
 
 #include <cmath>
 
-
-#include <Debug/AlbaDebug.hpp>
-
 using namespace alba::ThreeDimensions;
 using namespace std;
 
-void graphPoints(Points const& points, string const& nameOfGraph)
+void graphPoints(Gnuplot & gp, Points const& points, string const& nameOfGraph)
 {
-    Gnuplot gp;
-    gp << "splot ";
+    vector<pair<pair<double, double>, double>> pts;
+    for(unsigned int i=0; i<points.size(); i++)
     {
-        vector<pair<pair<double, double>, double>> pts;
-        for(unsigned int i=0; i<points.size(); i++)
-        {
-            pts.emplace_back(make_pair(make_pair(points[i].getX(), points[i].getY()), points[i].getZ()));
-        }
-        gp << gp.binFile1d(pts, "record") << "with lines title '"<< nameOfGraph <<"'";
+        pts.emplace_back(make_pair(make_pair(points[i].getX(), points[i].getY()), points[i].getZ()));
     }
-    gp << endl;
+    gp << gp.binFile1d(pts, "record") << "with lines title '"<< nameOfGraph <<"'";
+    gp << ", ";
+}
+
+void graphPoint(Gnuplot & gp, Point const& point, string const& nameOfGraph)
+{
+    vector<pair<pair<double, double>, double>> pts;
+    pts.emplace_back(make_pair(make_pair(point.getX(), point.getY()), point.getZ()));
+    gp << gp.binFile1d(pts, "record") << "with points palette pointsize 2 pointtype 2 title '"<< nameOfGraph <<"'";
+    gp << ", ";
 }
 
 
 TEST(AprgGeometryThreeDimensionsTest, DISABLED_LineWithSlope)
 {
+    Gnuplot gp;
+    gp << "splot ";
     Line line(Point(0,0,0), Point(2,3,-4));
     Points points;
     for(double x=-10; x<10; x+=0.1)
     {
-        ALBA_PRINT3(x, line.calculateYFromX(x), line.calculateZFromX(x));
         points.emplace_back(x, line.calculateYFromX(x), line.calculateZFromX(x));
     }
-    graphPoints(points, "LineWithSlope");
+    graphPoints(gp, points, "LineWithSlope");
+    gp << endl;
 }
 
-TEST(AprgGeometryThreeDimensionsTest, SamplePlane)
+TEST(AprgGeometryThreeDimensionsTest, DISABLED_LineWithSlopeWithPoints)
 {
-    Plane plane(Point(5,-2,6), Point(0,0,0), Point(2,3,-4));
-    Points points;
+    Gnuplot gp;
+    //Gnuplot gp(stdout); //for debugging
+    gp << "splot ";
+    Line line1(Point(5,-2,6), Point(1,1,1));
+    Line line2(Point(1,1,1), Point(2,3,-4));
+    Line line3(Point(5,-2,6), Point(2,3,-4));
+    //Line line3(Point(5,-2,6), Point(2,3,-4));
+    Points points1;
+    Points points2;
+    Points points3;
     for(double x=-10; x<10; x+=0.1)
     {
-        for(double y=-10; y<10; y+=0.1)
+        points1.emplace_back(x, line1.calculateYFromX(x), line1.calculateZFromX(x));
+        points2.emplace_back(x, line2.calculateYFromX(x), line2.calculateZFromX(x));
+        points3.emplace_back(x, line3.calculateYFromX(x), line3.calculateZFromX(x));
+    }
+    graphPoints(gp, points1, "Line1");
+    graphPoints(gp, points2, "Line2");
+    graphPoints(gp, points3, "Line3");
+    graphPoint(gp, Point(5,-2,6), "Point1");
+    graphPoint(gp, Point(1,1,1), "Point2");
+    graphPoint(gp, Point(2,3,-4), "Point3");
+    gp << endl;
+    Plane plane(Point(5,-2,6), Point(1,1,1), Point(2,3,-4));
+    //system("pause"); // for debugging
+}
+
+TEST(AprgGeometryThreeDimensionsTest, DISABLED_SamplePlane)
+{
+    Point point1(1,0,1);
+    Point point2(-2,0,2);
+    Point point3(2,0,-1);
+    Gnuplot gp;
+    gp << "splot ";
+    Plane plane(point1, point2, point3);
+    Points points;
+    for(double x=-10; x<10; x+=1)
+    {
+        for(double z=-10; z<10; z+=1)
         {
-            points.emplace_back(x, y, plane.calculateZFromXAndY(x, y));
+            points.emplace_back(x, plane.calculateYFromXAndZ(x, z), z);
         }
     }
-    graphPoints(points, "SamplePlane");
+    graphPoints(gp, points, "SamplePlane");
+    graphPoint(gp, point1, "Point1");
+    graphPoint(gp, point2, "Point2");
+    graphPoint(gp, point3, "Point3");
+    gp << endl;
 }
--- GnuplotIostream/GnuplotIostream/tst/AprgGeometryTwoDimensions_unit.cpp
+++ GnuplotIostream/GnuplotIostream/tst/AprgGeometryTwoDimensions_unit.cpp
@@ -2,40 +2,64 @@
 
 #include <TwoDimensions/Circle.hpp>
 #include <TwoDimensions/Hyperbola.hpp>
+#include <TwoDimensions/Line.hpp>
+#include <TwoDimensions/TwoDimensionsHelper.hpp>
 
 #include <gtest/gtest.h>
-
 #include <cmath>
 
 using namespace alba::TwoDimensions;
+using namespace alba::TwoDimensions::twoDimensionsHelper;
 using namespace std;
 
-void graphPoints(Points const& points, string const& nameOfGraph)
+void graphPoints(Gnuplot & gp, Points const& points, string const& nameOfGraph)
 {
-    Gnuplot gp;
-    gp << "plot ";
+    vector<pair<double, double>> pts;
+    for(unsigned int i=0; i<points.size(); i++)
     {
-        vector<pair<double, double>> pts;
-        for(unsigned int i=0; i<points.size(); i++)
-        {
-            pts.emplace_back(make_pair(points[i].getX(), points[i].getY()));
-        }
-        gp << gp.binFile1d(pts, "record") << "with lines title '"<< nameOfGraph <<"'";
+        pts.emplace_back(make_pair(points[i].getX(), points[i].getY()));
     }
-    gp << endl;
+    gp << gp.binFile1d(pts, "record") << "with lines title '"<< nameOfGraph <<"'";
+    gp << ", ";
 }
 
 
 TEST(AprgGeometryTwoDimensionsTest, DISABLED_CircleAtOriginWithRadius)
 {
+    Gnuplot gp;
+    gp << "plot ";
     Circle circle(Point(0,0), 3);
     Points points(circle.getPointsForCircumference(0.001));
-    graphPoints(points, "CircleAtOriginWithRadius");
+    graphPoints(gp, points, "CircleAtOriginWithRadius");
+    gp << endl;
 }
 
 TEST(HyperbolaTest, DISABLED_HyperbolaAtOriginWithRadius)
 {
+    Gnuplot gp;
+    gp << "plot ";
     Hyperbola hyperbola(Point(0, 0), 3, 2);
     Points points(hyperbola.getPointsForShape(0.001));
-    graphPoints(points, "HyperbolaAtOriginWithRadius");
+    graphPoints(gp, points, "HyperbolaAtOriginWithRadius");
+    gp << endl;
+}
+
+TEST(AprgGeometryTwoDimensionsTest, SampleCircle)
+{
+    Gnuplot gp;
+    gp << "plot ";
+    Circle circle(Point(1,2), 3);
+    Line expectedLine1(getTangentLineAt(circle, Point(1,5)));
+    Line expectedLine2(getTangentLineAt(circle, Point(4,2)));
+    Line expectedLine3(getTangentLineAt(circle, Point(2.5,3.5)));
+
+    Points pointsCircle(circle.getPointsForCircumference(0.01));
+    Points pointsLine1(expectedLine1.getPoints(Point(-10, -10), Point(10, 10), 0.001));
+    Points pointsLine2(expectedLine2.getPoints(Point(-10, -10), Point(10, 10), 0.001));
+    Points pointsLine3(expectedLine3.getPoints(Point(-10, -10), Point(10, 10), 0.001));
+    graphPoints(gp, pointsCircle, "Circle");
+    graphPoints(gp, pointsLine1, "Line1");
+    graphPoints(gp, pointsLine2, "Line2");
+    graphPoints(gp, pointsLine3, "Line3");
+    gp << endl;
 }
