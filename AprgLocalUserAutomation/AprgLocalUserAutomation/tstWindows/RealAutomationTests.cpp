e24a18a150956d9ea1e425be652c7f0215059114
mevalba <markearvinalba@gmail.com>
2021-03-31 18:45:25 +0800

    Integrated real automation tests.

--- AprgLocalUserAutomation/AprgLocalUserAutomation/tstWindows/RealAutomationTests.cpp
+++ AprgLocalUserAutomation/AprgLocalUserAutomation/tstWindows/RealAutomationTests.cpp
@@ -13,29 +13,25 @@ using namespace std;
 TEST(RealAutomationTest, NormalizeAudioForMp3Files) //DISABLED_
 {
     AlbaWindowsUserAutomation userAutomation;
-    AlbaLocalPathHandler mp3FilesPathHandler(R"(N:\MUSIC\AnimeSongsImproved)");
+    AlbaLocalPathHandler mp3FilesPathHandler(R"(N:\MUSIC\AnimeSongs2)");
 
     while(1)
-    {
-        if(userAutomation.isLetterPressed('s'))  //s for start
+    {        if(userAutomation.isLetterPressed('s'))  //s for start
         {
             ListOfPaths filePaths;
-            ListOfPaths directoryPaths;
-            mp3FilesPathHandler.findFilesAndDirectoriesOneDepth("*.*", filePaths, directoryPaths);
+            ListOfPaths directoryPaths;            mp3FilesPathHandler.findFilesAndDirectoriesOneDepth("*.*", filePaths, directoryPaths);
 
             for(string const& filePath : filePaths)
             {
 
                 AlbaLocalPathHandler filePathHandler(filePath);
-                if(filePathHandler.getExtension() == "mp3" && filePathHandler.getFileSizeEstimate() < 20000000)
+                if(filePathHandler.getExtension() == "mp3" && filePathHandler.getFileSizeEstimate() < 100000000) //100MB
                 {
                     //close previous file
-                    userAutomation.typeControlAndLetterSimultaneously('W');
-                    userAutomation.typeKey(VK_RIGHT);
+                    userAutomation.typeControlAndLetterSimultaneously('W');                    userAutomation.typeKey(VK_RIGHT);
                     userAutomation.typeKey(VK_RETURN);
 
-                    //open file
-                    userAutomation.typeControlAndLetterSimultaneously('O');
+                    //open file                    userAutomation.typeControlAndLetterSimultaneously('O');
                     userAutomation.sleep(1000);
 
                     //paste file name
@@ -46,14 +42,12 @@ TEST(RealAutomationTest, NormalizeAudioForMp3Files) //DISABLED_
                     userAutomation.typeKey(VK_RETURN);
 
                     //wait for the file to load
-                    userAutomation.sleep(10000);
+                    userAutomation.sleep(20000);
 
                     //select all track
                     userAutomation.typeControlAndLetterSimultaneously('A');
-
                     //click effect
                     userAutomation.doDoubleLeftClickAt(MousePosition(344,33));
-
                     //click normalization
                     userAutomation.doDoubleLeftClickAt(MousePosition(433, 443));
 
@@ -61,15 +55,13 @@ TEST(RealAutomationTest, NormalizeAudioForMp3Files) //DISABLED_
                     userAutomation.typeKey(VK_RETURN);
 
                     //wait for normalization process
-                    userAutomation.sleep(10000);
+                    userAutomation.sleep(20000);
 
                     //export
-                    userAutomation.pressDownKey(VK_CONTROL);
-                    userAutomation.pressDownKey(VK_SHIFT);
+                    userAutomation.pressDownKey(VK_CONTROL);                    userAutomation.pressDownKey(VK_SHIFT);
                     userAutomation.pressDownKey('E');
                     userAutomation.sleepWithRealisticDelay();
-                    userAutomation.pressUpKey('E');
-                    userAutomation.pressUpKey(VK_SHIFT);
+                    userAutomation.pressUpKey('E');                    userAutomation.pressUpKey(VK_SHIFT);
                     userAutomation.pressUpKey(VK_CONTROL);
 
                     //type enter key
@@ -80,15 +72,13 @@ TEST(RealAutomationTest, NormalizeAudioForMp3Files) //DISABLED_
                     userAutomation.sleep(1000);
 
                     //wait for export process
-                    userAutomation.sleep(10000);
+                    userAutomation.sleep(20000);
 
                     //close file
-                    userAutomation.typeControlAndLetterSimultaneously('W');
-                    userAutomation.typeKey(VK_RIGHT);
+                    userAutomation.typeControlAndLetterSimultaneously('W');                    userAutomation.typeKey(VK_RIGHT);
                     userAutomation.typeKey(VK_RETURN);
 
-                    //break;
-                }
+                    //break;                }
                 else
                 {
                     ALBA_PRINT2(filePath, filePathHandler.getFileSizeEstimate());
