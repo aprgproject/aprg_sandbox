fbed6e36c262182ab432b52d4353c00c76299901
mevalba <markearvinalba@gmail.com>
2021-04-18 18:12:55 +0800

    Addressed issues in WCDMATools.

--- WcdmaTools/WcdmaTools/src/ProgressBarThread.hpp
+++ WcdmaTools/WcdmaTools/src/ProgressBarThread.hpp
@@ -1,15 +1,12 @@
 #pragma once
 
 #include <Optional/AlbaOptional.hpp>
-#include <WcdmaToolsConfiguration.hpp>
 
 #include <QMutex>
-#include <QThread>
-#include <QWaitCondition>
+#include <QThread>#include <QWaitCondition>
 
 namespace wcdmaToolsGui
 {
-
 class QImage;
 
 class ProgressBarThread : public QThread
