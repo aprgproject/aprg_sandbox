754559fd24119a156d0dd69d52cce9987174112e
mevalba <markearvinalba@gmail.com>
2021-04-26 09:08:07 +0800

    Fixed CMake and Qt issues in WCDMATools.

--- WcdmaTools/WcdmaTools/WcdmaTools.pro
+++ WcdmaTools/WcdmaTools/WcdmaTools.pro
@@ -112,10 +112,9 @@ DEFINES += PATH_OF_7Z_EXECUTABLE=\\\"$$PWD/../../AprgFileExtractor/7z32/7z.exe\\
 DEFINES += PATH_OF_7Z_TEMP_FILE=\\\"$$PWD/../../AprgFileExtractor/7z32/TempFile.txt\\\"
 DEFINES += OS_WINDOWS=\\\"WIN32\\\"
 DEFINES += STATIC
+DEFINES += "APRG_CODE_VERSION=\"\\\"$$system(svnversion -n)\\\"\""
 
 CONFIG += c++11 console
-
 RESOURCES += \
     resources.qrc
-
 RC_FILE = icon/resource.rc
--- WcdmaTools/WcdmaTools/src/main.cpp
+++ WcdmaTools/WcdmaTools/src/main.cpp
@@ -5,17 +5,19 @@
 
 #include <QApplication>
 
+#include <iostream>
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    cout<<"WcdmaTools created by ARPG"<<endl;
+    cout<<"APRG_CODE_VERSION: "<<APRG_CODE_VERSION<<endl;
     QApplication a(argc, argv);
     WcdmaTools w;
-    if(argc==2)
-    {
+    if(argc==2)    {
         w.setInputFileOrDirectory(alba::stringHelper::getStringWithoutCharAtTheEnd(string(argv[1]), '"'));
     }
     w.show();
-
     return a.exec();
 }
