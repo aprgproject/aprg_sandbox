0703cc5537ebf5ce9225d836189d4988e9d00e71
mevalba <markearvinalba@gmail.com>
2021-06-20 20:35:43 +0800

    Implemented further functionalities in AprgEquation.

--- AprgEquation/AprgEquation/src/Equation/Constructs/BrentMethod.cpp
+++ AprgEquation/AprgEquation/src/Equation/Constructs/BrentMethod.cpp
@@ -44,15 +44,15 @@ AlbaNumberOptional BrentMethod::calculateRoot(AlbaNumber const& start, AlbaNumbe
 
     bool mflag(true);
 
-    unsigned int maxIterations = 100;
+    unsigned int const maxIterations = 1000;
+    double aPreviousValue=b.getDouble();
+    double bPreviousValue=a.getDouble();
     for(unsigned numberOfIterations=0; numberOfIterations<maxIterations; numberOfIterations++) //or |b − a| is small enough (convergence)
     {
-        if(calculate(s) == 0)
-        {
+        if(calculate(s) == 0)        {
             result.setValue(s);
             break;
-        }
-        if(calculate(b) == 0)
+        }        if(calculate(b) == 0)
         {
             result.setValue(b);
             break;
@@ -76,18 +76,12 @@ AlbaNumberOptional BrentMethod::calculateRoot(AlbaNumber const& start, AlbaNumbe
             }
             s = sOptional.getConstReference();
         }
-        else if(a == b)
-        {
-            break;
-        }
         if(isBisectionMethodNeeded(a,b,c,d,s,mflag))
         {
-            s = calculateBiSectionMethod(a, b);
-            mflag = true;
+            s = calculateBiSectionMethod(a, b);            mflag = true;
         }
         else
-        {
-            mflag = false;
+        {            mflag = false;
         }
         AlbaNumber fs = calculate(s);
         d = c;
@@ -108,14 +102,18 @@ AlbaNumberOptional BrentMethod::calculateRoot(AlbaNumber const& start, AlbaNumbe
             fa = calculate(a);
             fb = calculate(b);
         }
+        if(a.getDouble() == aPreviousValue && b.getDouble() == bPreviousValue)
+        {
+            break;
+        }
+        aPreviousValue = a.getDouble();
+        bPreviousValue = b.getDouble();
     }
 
-    if(result.hasContent() && !m_coefficients.empty())
-    {
+    if(result.hasContent() && !m_coefficients.empty())    {
         AlbaNumber aCoefficient(m_coefficients.front());
         if(aCoefficient.isIntegerOrFractionType())
-        {
-            AlbaNumber integerValue(result.getConstReference()*aCoefficient);
+        {            AlbaNumber integerValue(result.getConstReference()*aCoefficient);
             integerValue.convertToInteger();
             result.setValue(integerValue/aCoefficient);
         }
