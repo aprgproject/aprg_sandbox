8fdec28c250f269dc8a2e5383ef4043f500e4be4
mevalba <markearvinalba@gmail.com>
2021-06-22 15:44:26 +0800

    Extended capabilities of AprgEquation.

--- AprgEquation/AprgEquation/tst/Equation/Factorization/Factorization_unit.cpp
+++ AprgEquation/AprgEquation/tst/Equation/Factorization/Factorization_unit.cpp
@@ -1,11 +1,11 @@
 #include <Equation/Factorization/Factorization.hpp>
+#include <Equation/Term/Term.hpp>
+#include <Equation/Utilities.hpp>
 
 #include <gtest/gtest.h>
-
 using namespace std;
 
-namespace alba
-{
+namespace alba{
 
 namespace equation
 {
@@ -13,14 +13,33 @@ namespace equation
 namespace Factorization
 {
 
+TEST(ExpressionTest, FactorizeWorks)
+{
+    Polynomial polynomial1({Monomial(2, {{"x", 2}}), Monomial(5, {{"x", 1}}), Monomial(-12, {})});
+    Polynomial polynomial2({Monomial(4, {{"x", 2}}), Monomial(-4, {{"x", 1}}), Monomial(-3, {})});
+    Expression expressionToTest(createExpressionIfPossible(Terms{Term(polynomial1), Term("/"), Term(polynomial2)}));
+
+    Expression expressionToVerify(factorize(expressionToTest));
+
+    Polynomial polynomialToExpect1({Monomial(2, {{"x", 1}}), Monomial(-3, {})});
+    Polynomial polynomialToExpect2({Monomial(1, {{"x", 1}}), Monomial(4, {})});
+    Polynomial polynomialToExpect3({Monomial(2, {{"x", 1}}), Monomial(1, {})});
+    Expression expressionToExpect(createExpressionIfPossible(
+                                      Terms{
+                                          Term(polynomialToExpect1),
+                                          Term("*"), Term(polynomialToExpect2),
+                                          Term("/"), Term(polynomialToExpect1),
+                                          Term("/"), Term(polynomialToExpect3)
+                                      }));
+    EXPECT_EQ(expressionToExpect, expressionToVerify);
+}
+
 TEST(FactorizationTest, CommonConstantCanBeFactored_FactorizeWorks)
 {
     Polynomial polynomialToTest{Monomial(4, {{"x", 1}}), Monomial(20, {})};
-
     Polynomials polynomialsToVerify(factorize(polynomialToTest));
 
-    ASSERT_EQ(2u, polynomialsToVerify.size());
-    Polynomial polynomialToExpect1{Monomial(4, {})};
+    ASSERT_EQ(2u, polynomialsToVerify.size());    Polynomial polynomialToExpect1{Monomial(4, {})};
     Polynomial polynomialToExpect2{Monomial(1, {{"x", 1}}), Monomial(5, {})};
     EXPECT_EQ(polynomialToExpect1, polynomialsToVerify.at(0));
     EXPECT_EQ(polynomialToExpect2, polynomialsToVerify.at(1));
