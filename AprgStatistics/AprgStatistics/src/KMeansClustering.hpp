44c50356ae6e7cd4f6d89b7d9c6aa46a71d5fdfb
mevalba <markearvinalba@gmail.com>
2021-07-29 17:13:59 +0800

    Implemented STL container reservation in AprgStatistics.

--- AprgStatistics/AprgStatistics/src/KMeansClustering.hpp
+++ AprgStatistics/AprgStatistics/src/KMeansClustering.hpp
@@ -36,13 +36,12 @@ public:
 
     void addSamples(Samples samples)
     {
+        m_samples.reserve(m_samples.size() + samples.size());
         std::copy(samples.cbegin(), samples.cend(), std::back_inserter(m_samples));
     }
-
     Samples getSamples() const
     {
-        return m_samples;
-    }
+        return m_samples;    }
 
     GroupOfSamples getGroupOfSamplesUsingKMeans(unsigned int const numberOfGroups) const
     {
