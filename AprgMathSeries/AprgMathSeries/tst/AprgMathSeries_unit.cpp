e2c705baec0674db7483fc660169ffba8e791c57
mevalba <markearvinalba@gmail.com>
2021-07-29 14:10:40 +0800

    Enhanced AprgMathSeries module with new functionalities.

---
+++ AprgMathSeries/AprgMathSeries/tst/AprgMathSeries_unit.cpp
@@ -0,0 +1,16 @@
+#include <SimplestDesignToCopyForLibrariesAndPrograms.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+namespace alba
+{
+
+TEST(SampleTest, DISABLED_SampleTest1)
+{
+    SimplestDesignToCopyForLibrariesAndPrograms entity;
+}
+
+}
+
