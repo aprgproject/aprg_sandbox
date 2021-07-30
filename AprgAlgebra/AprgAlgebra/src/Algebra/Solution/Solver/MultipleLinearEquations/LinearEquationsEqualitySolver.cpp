8b494a4f3665f4903b23b9622d812cce9288904b
mevalba <markearvinalba@gmail.com>
2021-07-30 11:10:05 +0800

    Introduced new functionalities to AprgAlgebra module.

---
+++ AprgAlgebra/AprgAlgebra/src/Algebra/Solution/Solver/MultipleLinearEquations/LinearEquationsEqualitySolver.cpp
@@ -0,0 +1,127 @@
+#include "LinearEquationsEqualitySolver.hpp"
+
+#include <Algebra/Equation/EquationUtilities.hpp>
+#include <Algebra/Term/Utilities/ConvertHelpers.hpp>
+#include <Algebra/Term/Utilities/CreateHelpers.hpp>
+#include <Algebra/Term/Utilities/RetrieveHelpers.hpp>
+
+#include <algorithm>
+
+using namespace std;
+
+namespace alba
+{
+
+namespace algebra
+{
+
+LinearEquationsEqualitySolver::LinearEquationsEqualitySolver()
+    : BaseSolver()
+{}
+
+VariableNameToSolutionSetMap LinearEquationsEqualitySolver::calculateSolutionAndReturnSolutionSet(
+        Equations const& equations)
+{
+    VariableNameToSolutionSetMap solutionSets;
+    calculateSolution(solutionSets, equations);
+    return solutionSets;
+}
+
+VariableNameToSolutionSetMap LinearEquationsEqualitySolver::calculateSolutionAndReturnSolutionSet(
+        Polynomials const& polynomials)
+{
+    VariableNameToSolutionSetMap solutionSets;
+    calculateSolution(solutionSets, polynomials);
+    return solutionSets;
+}
+
+void LinearEquationsEqualitySolver::calculateSolution(
+        VariableNameToSolutionSetMap & solutionSets,
+        Equations const& equations)
+{
+    if(doesAllEquationsHaveEqualityOperator(equations))
+    {
+        Polynomials polynomials;
+        for(Equation const& equation : equations)
+        {
+            Equation simplifiedEquation(equation);
+            simplifiedEquation.simplify();
+            Term const& nonZeroLeftHandTerm(simplifiedEquation.getLeftHandTerm());
+            if(canBeConvertedToPolynomial(nonZeroLeftHandTerm))
+            {
+                polynomials.emplace_back(createPolynomialIfPossible(nonZeroLeftHandTerm));
+            }
+        }
+        calculateSolution(solutionSets, polynomials);
+    }
+}
+
+void LinearEquationsEqualitySolver::calculateSolution(
+        VariableNameToSolutionSetMap & solutionSets,
+        Polynomials const& polynomials)
+{
+    VariableNamesSet variables;
+    AlbaNumbersSet exponents;
+    retrieveExponents(exponents, polynomials);
+    retrieveVariableNames(variables, polynomials);
+    if(areExponentsEqualToOneAndZero(exponents)
+            && variables.size() == polynomials.size())
+    {
+        NumberMatrix coefficientsMatrix(variables.size()+1, polynomials.size());
+        setMatrixCoefficients(coefficientsMatrix, variables, polynomials);
+        coefficientsMatrix.transformToReducedEchelonForm();
+        if(coefficientsMatrix.isReducedRowEchelonForm())
+        {
+            saveSolutionSetsFromTheCoefficientMatrix(solutionSets, coefficientsMatrix, variables);
+            setAsCompleteSolution();
+        }
+    }
+}
+
+bool LinearEquationsEqualitySolver::areExponentsEqualToOneAndZero(
+        AlbaNumbersSet const& exponents) const
+{
+    return all_of(exponents.cbegin(), exponents.cend(), [](AlbaNumber const& exponent)
+    {
+        return exponent == 1 || exponent == 0;
+    });
+}
+
+void LinearEquationsEqualitySolver::setMatrixCoefficients(
+        LinearEquationsEqualitySolver::NumberMatrix & coefficientsMatrix,
+        VariableNamesSet const& variableNames,
+        Polynomials const& polynomials)
+{
+    unsigned int rowIndex=0;
+    for(Polynomial const& polynomial : polynomials)
+    {
+        unsigned int columnIndex=0;
+        for(string const& variableName : variableNames)
+        {
+            coefficientsMatrix.set(columnIndex++, rowIndex, getCoefficientForVariableOnly(variableName, polynomial));
+        }
+        coefficientsMatrix.set(columnIndex, rowIndex++, getCoefficientWithNoVariables(polynomial));
+    }
+}
+
+void LinearEquationsEqualitySolver::saveSolutionSetsFromTheCoefficientMatrix(
+        VariableNameToSolutionSetMap & solutionSets,
+        NumberMatrix const& coefficientsMatrix,
+        VariableNamesSet const& variables)
+{
+    unsigned int index=0;
+    unsigned int columnEndIndex = variables.size();
+    for(string const& variableName : variables)
+    {
+        AlbaNumber identityDiagonalIndex(coefficientsMatrix.get(index, index));
+        if(identityDiagonalIndex == 1)
+        {
+            solutionSets[variableName].addAcceptedValue(-coefficientsMatrix.get(columnEndIndex, index));
+        }
+        index++;
+    }
+}
+
+}
+
+}
---
+++ AprgAlgebra/AprgAlgebra/src/Algebra/Solution/Solver/MultipleLinearEquations/LinearEquationsEqualitySolver.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <Algebra/Equation/Equation.hpp>
+#include <Algebra/Solution/SolutionSet/SolutionSet.hpp>
+#include <Algebra/Solution/Solver/BaseSolver.hpp>
+#include <Algebra/Term/TermTypes/Polynomial.hpp>
+#include <Algebra/Term/TermTypes/TermContainerTypes.hpp>
+#include <Matrix/Matrix.hpp>
+
+namespace alba
+{
+
+namespace algebra
+{
+
+class LinearEquationsEqualitySolver : public BaseSolver
+{
+public:
+    using NumberMatrix=Matrix<AlbaNumber>;
+    LinearEquationsEqualitySolver();
+
+    VariableNameToSolutionSetMap calculateSolutionAndReturnSolutionSet(Equations const& equations);
+    VariableNameToSolutionSetMap calculateSolutionAndReturnSolutionSet(Polynomials const& polynomials);
+
+private:
+    void calculateSolution(VariableNameToSolutionSetMap & solutionSets, Equations const& equations);
+    void calculateSolution(VariableNameToSolutionSetMap & solutionSets, Polynomials const& polynomials);
+    bool areExponentsEqualToOneAndZero(AlbaNumbersSet const& exponents) const;
+    void setMatrixCoefficients(
+            NumberMatrix & coefficientsMatrix,
+            VariableNamesSet const& variableNames,
+            Polynomials const& polynomials);
+    void saveSolutionSetsFromTheCoefficientMatrix(
+            VariableNameToSolutionSetMap & solutionSets,
+            NumberMatrix const& coefficientsMatrix,
+            VariableNamesSet const& variables);
+};
+
+
+}
+
+}
