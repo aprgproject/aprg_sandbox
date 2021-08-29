0bb48e197facf9ef56f6eaa8f296966cbd0545a5
mevalba <markearvinalba@gmail.com>
2021-08-29 08:21:44 +0800

    Performed refactoring in AprgProcessFiles module.

--- AprgProcessFiles/AprgProcessFiles/src/CropFile/AlbaCropFile.hpp
+++ AprgProcessFiles/AprgProcessFiles/src/CropFile/AlbaCropFile.hpp
@@ -22,15 +22,13 @@ public:
     AlbaCropFile(std::string const& prioritizedLineCondition, double const cropSize);
     AlbaCropFile(std::string const& prioritizedLineCondition, double const cropSize, UpdateFunctionWithPercentage const& updateFunctionAfterOneIteration);
     bool isOutputFileWritten() const;
-    void processFile( std::string const& inputFilePath, std::string const& outputFilePath);
+    void processFile(std::string const& inputFilePath, std::string const& outputFilePath);
 
 private:
-    double getLocationOfPrioritizedPrint(std::string const& inputFilePath);
-    void performCropForFile(std::string const& inputFilePath, std::string const& outputFilePath, double const foundLocation);
+    double getLocationOfPrioritizedPrint(std::string const& inputFilePath);    void performCropForFile(std::string const& inputFilePath, std::string const& outputFilePath, double const foundLocation);
     LocationsInFile getLocationsInFile(double const foundLocation, double const fileSize) const;
     void updateAfterOneIteration(double const percentage);
-    bool m_isOutputFileWritten;
-    double m_cropSize;
+    bool m_isOutputFileWritten;    double m_cropSize;
     AlbaGrepStringEvaluator m_prioritizedLineEvaluator;
     AlbaOptional<UpdateFunctionWithPercentage const&> m_updateFunctionAfterOneIterationOptional;
 };
