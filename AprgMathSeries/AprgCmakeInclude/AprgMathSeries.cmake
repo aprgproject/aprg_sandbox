ba1c5571078eaa56e7998446cc4865c044689465
mevalba <markearvinalba@gmail.com>
2021-11-01 20:56:26 +0800

    Removed obsolete aprgmathseries module.

--- AprgMathSeries/AprgCmakeInclude/AprgMathSeries.cmake
+++
@@ -1,8 +0,0 @@
-#Assign AprgCommon directory, this needs aprg directory
-set(APRG_MATH_SERIES_DIR ${APRG_DIR}/SimplestDesignToCopyForLibrariesAndPrograms/SimplestDesignToCopyForLibrariesAndPrograms/)
-
-#Include APRG_COMMON files and directories
-include(${APRG_MATH_SERIES_DIR}/cmake/FilesAndDirectories.cmake)
-
-include_directories(${APRG_MATH_SERIES_INCLUDE_DIRECTORIES})
-add_library(APRG_MATH_SERIES ${APRG_MATH_SERIES_SOURCES})
