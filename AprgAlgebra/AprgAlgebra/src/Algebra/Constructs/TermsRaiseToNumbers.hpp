bc4bcfc9c2287f2f5ec8b6d30d43b745d99c45bb
mevalba <markearvinalba@gmail.com>
2021-11-20 15:31:56 +0800

    Integrated more functionalities into aprgalgebra.

--- AprgAlgebra/AprgAlgebra/src/Algebra/Constructs/TermsRaiseToNumbers.hpp
+++ AprgAlgebra/AprgAlgebra/src/Algebra/Constructs/TermsRaiseToNumbers.hpp
@@ -1,51 +1,20 @@
 #pragma once
 
-#include <Algebra/Term/TermTypes/Term.hpp>
-
-#include <functional>
-#include <map>
+#include <Algebra/Constructs/TermRaiseToANumber.hpp>
+#include <Algebra/Constructs/TermsRaiseToExponents.hpp>
 
 namespace alba
 {
-
 namespace algebra
 {
 
-class TermsRaiseToNumbers
+using TermsRaiseToNumbersBase = TermsRaiseToExponents<AlbaNumber, TermRaiseToANumber>;
+class TermsRaiseToNumbers : public TermsRaiseToNumbersBase
 {
 public:
-    using BaseToExponentMap = std::map<Term, AlbaNumber>;
-    using BaseExponentPair = std::pair<Term, AlbaNumber>;
-
     TermsRaiseToNumbers();
     TermsRaiseToNumbers(BaseToExponentMap const& baseToExponentMap);
-
-    BaseToExponentMap const& getBaseToExponentMap() const;
-    AlbaNumber getExponentOfBase(Term const& base) const;
-    Terms getTermsInMultiplicationOperation() const;
-    TermsWithDetails getTermWithDetailsInMultiplicationAndDivisionOperation() const;
-    Term getCombinedTerm() const;
-
-    void addExponents(TermsRaiseToNumbers const& termsRaiseToNumbers);
-    void subtractExponents(TermsRaiseToNumbers const& termsRaiseToNumbers);
-    void multiplyToExponents(AlbaNumber const& number);
-    void putTerm(Term const& term, TermAssociationType const association);
-    void putTerms(Terms const& terms, TermAssociationType const association);
-
-    void setBaseAndExponent(Term const& base, AlbaNumber const& exponent);
-    void removeItemWithBase(Term const& base);
-
-    void clear();
-    void simplify();
-
-private:
-
-    void removeItemsWithExponentsZero();
-    Term convertToTerm(Term const& base, AlbaNumber const& exponent) const;
-
-    BaseToExponentMap m_baseToExponentMap;
 };
 
 }
-
 }
--- AprgAlgebra/AprgAlgebra/src/Algebra/Constructs/TermsRaiseToTerms.hpp
+++ AprgAlgebra/AprgAlgebra/src/Algebra/Constructs/TermsRaiseToTerms.hpp
@@ -1,49 +1,20 @@
 #pragma once
 
-#include <Algebra/Term/TermTypes/Term.hpp>
-
-#include <map>
+#include <Algebra/Constructs/TermRaiseToTerms.hpp>
+#include <Algebra/Constructs/TermsRaiseToExponents.hpp>
 
 namespace alba
 {
-
 namespace algebra
 {
 
-class TermsRaiseToTerms
+using TermsRaiseToTermsBase = TermsRaiseToExponents<Term, TermRaiseToTerms>;
+class TermsRaiseToTerms : public TermsRaiseToTermsBase
 {
 public:
-    using BaseToExponentMap = std::map<Term, Term>;
-    using BaseExponentPair = std::pair<Term, Term>;
-
     TermsRaiseToTerms();
     TermsRaiseToTerms(BaseToExponentMap const& baseToExponentMap);
-
-    BaseToExponentMap const& getBaseToExponentMap() const;
-    Term getExponentOfBase(Term const& base) const;
-    Terms getTermsInMultiplicationOperation() const;
-    TermsWithDetails getTermWithDetailsInMultiplicationAndDivisionOperation() const;
-    Term getCombinedTerm() const;
-
-    void addExponents(TermsRaiseToTerms const& termsRaiseToNumbers);
-    void subtractExponents(TermsRaiseToTerms const& termsRaiseToNumbers);
-    void multiplyToExponents(Term const& term);
-    void putTerm(Term const& term, TermAssociationType const association);
-    void putTerms(Terms const& terms, TermAssociationType const association);
-
-    void setBaseAndExponent(Term const& base, Term const& exponent);
-    void removeItemWithBase(Term const& base);
-
-    void simplify();
-
-private:
-
-    void removeItemsWithExponentsZero();
-    Term convertToTerm(Term const& base, Term const& exponent) const;
-
-    BaseToExponentMap m_baseToExponentMap;
 };
 
 }
-
 }
