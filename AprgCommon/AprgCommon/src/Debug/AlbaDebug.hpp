60e3757eda83189d7d78355940515fa6286a9c45
mevalba <markearvinalba@gmail.com>
2022-01-25 10:17:46 +0800

    Enhanced albadebug functionality in aprgcommon.

--- AprgCommon/AprgCommon/src/Debug/AlbaDebug.hpp
+++ AprgCommon/AprgCommon/src/Debug/AlbaDebug.hpp
@@ -44,21 +44,19 @@ static std::ofstream debugStream(ALBA_PRINT_OUTPUT_STREAM_FILE_PATH);
 
 #define ALBA_PRINT_PRECISION(parameter) ALBA_PRINT_OUTPUT_STREAM.precision(parameter)
 #define ALBA_EXIT exit(0)
-#define ALBA_PRINT0(parameter) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT0 in " << __FUNCTION__ << "(...): " << parameter << endl
-#define ALBA_PRINT1(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT1 in " << __FUNCTION__ << "(...): " << Z_ALBA_PRIVATE_GET_STRING_PRINT1(__VA_ARGS__) << endl
-#define ALBA_PRINT2(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT2 in " << __FUNCTION__ << "(...): " << Z_ALBA_PRIVATE_GET_STRING_PRINT2(__VA_ARGS__) << endl
-#define ALBA_PRINT3(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT3 in " << __FUNCTION__ << "(...): " << Z_ALBA_PRIVATE_GET_STRING_PRINT3(__VA_ARGS__) << endl
-#define ALBA_PRINT4(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT4 in " << __FUNCTION__ << "(...): " << Z_ALBA_PRIVATE_GET_STRING_PRINT4(__VA_ARGS__) << endl
-#define ALBA_PRINT5(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT5 in " << __FUNCTION__ << "(...): " << Z_ALBA_PRIVATE_GET_STRING_PRINT5(__VA_ARGS__) << endl
-#define ALBA_PRINT6(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT6 in " << __FUNCTION__ << "(...): " << Z_ALBA_PRIVATE_GET_STRING_PRINT6(__VA_ARGS__) << endl
+#define ALBA_PRINT0(parameter) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT0 in " << __FUNCTION__ << "(...) in line:" << __LINE__ << " || " << parameter << endl
+#define ALBA_PRINT1(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT1 in " << __FUNCTION__ << "(...) in line:" << __LINE__ << " || " << Z_ALBA_PRIVATE_GET_STRING_PRINT1(__VA_ARGS__) << endl
+#define ALBA_PRINT2(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT2 in " << __FUNCTION__ << "(...) in line:" << __LINE__ << " || " << Z_ALBA_PRIVATE_GET_STRING_PRINT2(__VA_ARGS__) << endl
+#define ALBA_PRINT3(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT3 in " << __FUNCTION__ << "(...) in line:" << __LINE__ << " || " << Z_ALBA_PRIVATE_GET_STRING_PRINT3(__VA_ARGS__) << endl
+#define ALBA_PRINT4(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT4 in " << __FUNCTION__ << "(...) in line:" << __LINE__ << " || " << Z_ALBA_PRIVATE_GET_STRING_PRINT4(__VA_ARGS__) << endl
+#define ALBA_PRINT5(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT5 in " << __FUNCTION__ << "(...) in line:" << __LINE__ << " || " << Z_ALBA_PRIVATE_GET_STRING_PRINT5(__VA_ARGS__) << endl
+#define ALBA_PRINT6(...) ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT6 in " << __FUNCTION__ << "(...) in line:" << __LINE__ << " || " << Z_ALBA_PRIVATE_GET_STRING_PRINT6(__VA_ARGS__) << endl
 
 #define ALBA_PRINT_CONTAINER(container)  \
-    ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT_CONTAINER in " << __FUNCTION__ << "(...): " \
-    << ALBA_MACROS_GET_STRING_LITERAL(container) \
+    ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT_CONTAINER in " << __FUNCTION__ << "(...): " \    << ALBA_MACROS_GET_STRING_LITERAL(container) \
     << " (with size " << container.size() <<")" \
     << " : {"; \
-    for(auto const& item : container) \
-{ \
+    for(auto const& item : container) \{ \
     ALBA_PRINT_OUTPUT_STREAM << "[" << item << "], "; \
 } \
     ALBA_PRINT_OUTPUT_STREAM << "}" << endl;
