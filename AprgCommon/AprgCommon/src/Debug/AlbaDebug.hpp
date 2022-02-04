87320441e8229f61c03fcec84de7d54acbe51ed0
mevalba <markearvinalba@gmail.com>
2022-02-04 15:57:38 +0800

    Enhanced albadebug functionality in aprgcommon.

--- AprgCommon/AprgCommon/src/Debug/AlbaDebug.hpp
+++ AprgCommon/AprgCommon/src/Debug/AlbaDebug.hpp
@@ -31,21 +31,51 @@ static ofstream debugStream(ALBA_PRINT_OUTPUT_STREAM_FILE_PATH);
 
 
 // Print parameter functions
+template <typename ParameterPointerType>
+void printParameter(ostream & outputStream, string const& parameterName, ParameterPointerType const*const parameterPointer)
+{
+    if(parameterPointer == nullptr)
+    {
+        outputStream << parameterName << " : [nullptr] ";
+    }
+    else
+    {
+        outputStream << "*" << parameterName << " : [" << *parameterPointer << "] ";
+    }
+}
+
+template <>
 void printParameter(ostream & outputStream, string const& , char const*const parameter)
 {
-    outputStream << R"(")" << parameter << R"(" )";
+    outputStream << R"([")" << parameter << R"("] )";
+}
+
+template <typename ParameterType>
+void printParameter(ostream & outputStream, string const& parameterName, unique_ptr<ParameterType> const& parameterPointer)
+{
+    printParameter(outputStream, parameterName, parameterPointer.get());
+}
+
+template <typename ParameterType>
+void printParameter(ostream & outputStream, string const& parameterName, shared_ptr<ParameterType> const& parameterPointer)
+{
+    printParameter(outputStream, parameterName, parameterPointer.get());
+}
+
+template <typename ParameterType>
+void printParameter(ostream & outputStream, string const& parameterName, weak_ptr<ParameterType> const& parameterPointer)
+{
+    printParameter(outputStream, parameterName, parameterPointer.get());
 }
 
 template <typename ParameterType>
 void printParameter(ostream & outputStream, string const& parameterName, ParameterType const& parameter)
 {
-    outputStream << parameterName << " : [" << parameter << "] ";
-}
+    outputStream << parameterName << " : [" << parameter << "] ";}
 
 template <typename ValueType, size_t SIZE,
           template<typename, size_t> class Container>
-void printParameter(ostream & outputStream, string const& parameterName, Container<ValueType, SIZE> const& container)
-{
+void printParameter(ostream & outputStream, string const& parameterName, Container<ValueType, SIZE> const& container){
     outputStream << parameterName << "(with size " << container.size() << "):{";
     saveContentsToStream(outputStream, container, StreamFormat::String);
     outputStream << "} ";
@@ -92,16 +122,14 @@ void printParameter(ostream & outputStream, string const& parameterName, Contain
 
 // Macros to use
 #define ALBA_PRINT_DETAILS ALBA_PRINT_OUTPUT_STREAM << "ALBA_PRINT in line:" << setw(4) << __LINE__ << " in " << __FUNCTION__ << "(...): ";
-#define ALBA_PRINT1(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT1(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl;
-#define ALBA_PRINT2(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT2(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl;
-#define ALBA_PRINT3(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT3(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl;
-#define ALBA_PRINT4(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT4(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl;
-#define ALBA_PRINT5(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT5(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl;
-#define ALBA_PRINT6(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT6(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl;
+#define ALBA_PRINT1(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT1(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl
+#define ALBA_PRINT2(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT2(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl
+#define ALBA_PRINT3(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT3(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl
+#define ALBA_PRINT4(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT4(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl
+#define ALBA_PRINT5(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT5(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl
+#define ALBA_PRINT6(...) ALBA_PRINT_DETAILS Z_ALBA_PRIVATE_PRINT6(__VA_ARGS__) ALBA_PRINT_OUTPUT_STREAM << endl
 
 #define ALBA_PRINT_PRECISION(parameter) ALBA_PRINT_OUTPUT_STREAM.precision(parameter)
-
 #define ALBA_EXIT exit(0)
-
 
 }//namespace alba
--- AprgCommon/AprgCommon/tst/DebugTests/AlbaDebug_unit.cpp
+++ AprgCommon/AprgCommon/tst/DebugTests/AlbaDebug_unit.cpp
@@ -5,18 +5,24 @@
 #include <gtest/gtest.h>
 
 #include <map>
+#include <memory>
 
 using namespace std;
-
 namespace alba
 {
 
 TEST(AlbaDebugTest, DISABLED_PrintTest)
 {
-    map<unsigned int, char> container{{1U, 'A'}, {5U, 'E'}, {7U, 'G'}, {8U, 'H'}};
-    unsigned int singleParameter1=75, singleParameter2=76, singleParameter3=77;
+    unsigned int singleParameter1=75, singleParameter2=75, singleParameter3=75;
+    unique_ptr<unsigned int> uniquePointer(make_unique<unsigned int>(695));
+    unsigned int const* nullPointer = nullptr;
+    unsigned int const* rawPointer = uniquePointer.get();
+    vector<unsigned int> container1{454, 6, 512, 315};
+    map<unsigned int, char> container2{{1U, 'A'}, {5U, 'E'}, {7U, 'G'}, {8U, 'H'}};
 
-    ALBA_PRINT5("Const char* string literal", container, singleParameter1, singleParameter2, singleParameter3);
+    ALBA_PRINT4("Print for single parameters", singleParameter1, singleParameter2, singleParameter3);
+    ALBA_PRINT4("Print for pointer", nullPointer, rawPointer, uniquePointer);
+    ALBA_PRINT3("Print for containers", container1, container2);
 }
 
 }
