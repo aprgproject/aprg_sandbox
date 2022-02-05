fe99282f835363351af5d8aa7695a83c0c58455f
mevalba <markearvinalba@gmail.com>
2022-02-05 15:29:30 +0800

    Enhanced aprgcommon through refactoring.

---
+++ AprgCommon/AprgCommon/tstLinux/CommonTests/PathHandler/AlbaLinuxPathHandlerTest.cpp
@@ -0,0 +1,18 @@
+#include <Common/String/AlbaStringHelper.hpp>
+#include <CommonTests/PathHandler/AlbaLinuxPathHandlerTest.hpp>
+
+using namespace alba::stringHelper;
+using namespace std;
+
+namespace alba
+{
+
+AlbaLinuxPathHandlerTest::AlbaLinuxPathHandlerTest()
+{}
+
+string AlbaLinuxPathHandlerTest::convertToSimplestPath(string const& path)
+{
+    return getCorrectPathWithoutDoublePeriod<'/'>(getCorrectPathWithReplacedSlashCharacters<'/'>(path));
+}
+
+}
