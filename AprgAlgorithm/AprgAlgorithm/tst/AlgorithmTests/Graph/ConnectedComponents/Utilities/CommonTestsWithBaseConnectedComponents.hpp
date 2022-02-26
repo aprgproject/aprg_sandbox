cc03dd2017b724de01a2b15baab61a477e10c3a7
mevalba <markearvinalba@gmail.com>
2022-02-26 21:29:26 +0800

    Added advanced functionalities to aprgalgorithm.

--- AprgAlgorithm/AprgAlgorithm/tst/AlgorithmTests/Graph/ConnectedComponents/Utilities/CommonTestsWithBaseConnectedComponents.hpp
+++ AprgAlgorithm/AprgAlgorithm/tst/AlgorithmTests/Graph/ConnectedComponents/Utilities/CommonTestsWithBaseConnectedComponents.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <Algorithm/Graph/ConnectedComponents/BaseConnectedComponents.hpp>
+#include <Algorithm/Graph/UndirectedGraph/BaseUndirectedGraph.hpp>
 
 #include <gtest/gtest.h>
-
 namespace alba
 {
-
 namespace algorithm
 {
 
@@ -17,15 +16,14 @@ template<typename ConnectedComponentType, typename GraphType>
 void performIsConnectedTestWhenEmpty()
 {
     GraphType graph;
-    ConnectedComponentType connectedComponents(graph);
+    BaseUndirectedGraph & baseGraph(graph);
+    ConnectedComponentType connectedComponents(baseGraph);
     BaseConnectedComponents const& baseConnectedComponents(connectedComponents);
 
-    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 1U));
-    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 2U));
+    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 1U));    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 2U));
     EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 3U));
     EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 4U));
-    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 5U));
-    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 6U));
+    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 5U));    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 6U));
     EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 7U));
     EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 8U));
     EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 9U));
@@ -38,28 +36,27 @@ template<typename ConnectedComponentType, typename GraphType>
 void performIsConnectedTestWhenNotEmpty()
 {
     GraphType graph;
-    graph.connect(0U, 1U);
-    graph.connect(0U, 2U);
-    graph.connect(0U, 5U);
-    graph.connect(0U, 6U);
-    graph.connect(3U, 5U);
-    graph.connect(3U, 4U);
-    graph.connect(4U, 5U);
-    graph.connect(4U, 6U);
-    graph.connect(7U, 8U);
-    graph.connect(9U, 10U);
-    graph.connect(9U, 11U);
-    graph.connect(9U, 12U);
-    graph.connect(11U, 12U);
-    ConnectedComponentType connectedComponents(graph);
+    BaseUndirectedGraph & baseGraph(graph);
+    baseGraph.connect(0U, 1U);
+    baseGraph.connect(0U, 2U);
+    baseGraph.connect(0U, 5U);
+    baseGraph.connect(0U, 6U);
+    baseGraph.connect(3U, 5U);
+    baseGraph.connect(3U, 4U);
+    baseGraph.connect(4U, 5U);
+    baseGraph.connect(4U, 6U);
+    baseGraph.connect(7U, 8U);
+    baseGraph.connect(9U, 10U);
+    baseGraph.connect(9U, 11U);
+    baseGraph.connect(9U, 12U);
+    baseGraph.connect(11U, 12U);
+    ConnectedComponentType connectedComponents(baseGraph);
     BaseConnectedComponents const& baseConnectedComponents(connectedComponents);
 
-    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 7U));
-    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 9U));
+    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 7U));    EXPECT_FALSE(baseConnectedComponents.isConnected(0U, 9U));
     EXPECT_FALSE(baseConnectedComponents.isConnected(7U, 9U));
     EXPECT_TRUE(baseConnectedComponents.isConnected(0U, 1U));
-    EXPECT_TRUE(baseConnectedComponents.isConnected(0U, 2U));
-    EXPECT_TRUE(baseConnectedComponents.isConnected(0U, 3U));
+    EXPECT_TRUE(baseConnectedComponents.isConnected(0U, 2U));    EXPECT_TRUE(baseConnectedComponents.isConnected(0U, 3U));
     EXPECT_TRUE(baseConnectedComponents.isConnected(0U, 4U));
     EXPECT_TRUE(baseConnectedComponents.isConnected(0U, 5U));
     EXPECT_TRUE(baseConnectedComponents.isConnected(0U, 6U));
