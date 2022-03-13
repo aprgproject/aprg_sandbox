a851aeca1773623155329e150160367d31ca16f8
mevalba <markearvinalba@gmail.com>
2022-03-13 16:24:51 +0800

    Rectified cmake configuration related to curlcpp.

--- CurlCpp/AprgCmakeInclude/CurlCpp.cmake
+++ CurlCpp/AprgCmakeInclude/CurlCpp.cmake
@@ -1,12 +1,11 @@
 #Include curl library, this needs aprg directory
-include(${APRG_DIR}/curl-7.38.0/aprgCmakeInclude/curl.cmake)
+include(${APRG_DIR}/curl-7.38.0/AprgCmakeInclude/curl.cmake)
 
 #Assign curlCpp directory, this needs aprg directory
 set(CURL_CPP_DIR ${APRG_DIR}/CurlCpp/CurlCpp/)
 
 #Include CURL_CPP_SOURCES
-include(${CURL_CPP_DIR}/cmake/FilesAndDirectories.cmake)
+include(${CURL_CPP_DIR}/CMake/FilesAndDirectories.cmake)
 
 include_directories(${CURL_CPP_INCLUDE_DIRECTORIES})
-add_library(CURL_CPP ${CURL_CPP_SOURCES})
-target_link_libraries(CURL_CPP CURL_LIB)
+add_library(CURL_CPP ${CURL_CPP_SOURCES})target_link_libraries(CURL_CPP CURL_LIB)
