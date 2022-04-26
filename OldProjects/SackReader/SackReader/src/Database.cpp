4f8cf0657d207c3ae3177860c4774078e8df95c3
mevalba <markearvinalba@gmail.com>
2022-04-26 19:39:03 +0800

    Performed refactoring in 'sackreader'.

--- OldProjects/SackReader/SackReader/src/Database.cpp
+++ OldProjects/SackReader/SackReader/src/Database.cpp
@@ -1,41 +1,37 @@
 #include "Database.hpp"
 
-#include <Common/File/AlbaFileParameterReader.hpp>
-#include <Common/File/AlbaFileParameterWriter.hpp>
+#include <Common/Stream/AlbaStreamParameterReader.hpp>
+#include <Common/Stream/AlbaStreamParameterWriter.hpp>
 
 #include <algorithm>
+#include <fstream>
 
 using namespace std;
-
 namespace alba
 {
 
 void Database::saveDatabaseToFile(string const& path)
 {
     ofstream fileStream(path);
-    AlbaFileParameterWriter writer(fileStream);
+    AlbaStreamParameterWriter writer(fileStream);
     writer.writeMapData(fileToPathMap);
     writer.writeMapData(constantNameToConstantDetailsMap);
-    writer.writeMapData(messageNameToMessageDetailsMap);
-    writer.writeMapData(structureNameToStructureDetailsMap);
+    writer.writeMapData(messageNameToMessageDetailsMap);    writer.writeMapData(structureNameToStructureDetailsMap);
     writer.writeMapData(unionNameToUnionDetailsMap);
     writer.writeMapData(enumNameToEnumDetailsMap);
-    writer.writeMapData(typedefNameToTypedefDetailsMap);
-    writer.writeSetData(messagesToGenerate);
+    writer.writeMapData(typedefNameToTypedefDetailsMap);    writer.writeSetData(messagesToGenerate);
 }
 
 void Database::loadDatabaseFromFile(string const& path)
 {
     ifstream fileStream(path);
-    AlbaFileParameterReader reader(fileStream);
+    AlbaStreamParameterReader reader(fileStream);
     reader.readMapData(fileToPathMap);
     reader.readMapData(constantNameToConstantDetailsMap);
-    reader.readMapData(messageNameToMessageDetailsMap);
-    reader.readMapData(structureNameToStructureDetailsMap);
+    reader.readMapData(messageNameToMessageDetailsMap);    reader.readMapData(structureNameToStructureDetailsMap);
     reader.readMapData(unionNameToUnionDetailsMap);
     reader.readMapData(enumNameToEnumDetailsMap);
-    reader.readMapData(typedefNameToTypedefDetailsMap);
-    reader.readSetData(messagesToGenerate);
+    reader.readMapData(typedefNameToTypedefDetailsMap);    reader.readSetData(messagesToGenerate);
 }
 
 string Database::getFileFullPath(string const& fileName) const
