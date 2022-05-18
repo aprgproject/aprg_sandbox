a63b9af7869b4c66c910ca15c26d3ba1baf56b95
mevalba <markearvinalba@gmail.com>
2022-05-18 12:38:54 +0800

    Introduced advanced functionalities in 'aprgalgorithm'.

--- AprgAlgebra/AprgAlgebra/src/Algebra/Constructs/RationalizeTermOverTerm.cpp
+++ AprgAlgebra/AprgAlgebra/src/Algebra/Constructs/RationalizeTermOverTerm.cpp
@@ -57,27 +57,19 @@ void RationalizeTermOverTerm::rationalize(
         Term & termToRationalize,
         Term & otherTerm)
 {
-    unsigned int numberOfTermsLimit = static_cast<unsigned int>(getNumberOfTerms(termToRationalize) *1.5);
     Term rationalizedTerm, multiplier;
     retrieveTermsForRationalization(rationalizedTerm, multiplier, termToRationalize);
-    while(!multiplier.isEmpty())
-    {
+    while(!multiplier.isEmpty())    {
         termToRationalize = rationalizedTerm;
         otherTerm = otherTerm * multiplier;
         simplifyForRationalize(termToRationalize);
         simplifyForRationalize(otherTerm);
-        if(numberOfTermsLimit < getNumberOfTerms(termToRationalize))
-        {
-            break;
-        }
         rationalizedTerm.clear();
         multiplier.clear();
-        retrieveTermsForRationalization(rationalizedTerm, multiplier, termToRationalize);
-    }
+        retrieveTermsForRationalization(rationalizedTerm, multiplier, termToRationalize);    }
 }
 
-void RationalizeTermOverTerm::simplifyForRationalize(Term & term)
-{
+void RationalizeTermOverTerm::simplifyForRationalize(Term & term){
     SimplificationOfExpression::ConfigurationDetails rationalizeConfigurationDetails(
                 SimplificationOfExpression::Configuration::getInstance().getConfigurationDetails());
     rationalizeConfigurationDetails.shouldSimplifyToACommonDenominator = true;
