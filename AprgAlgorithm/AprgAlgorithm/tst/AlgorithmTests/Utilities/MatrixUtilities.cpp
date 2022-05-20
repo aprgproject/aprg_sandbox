014e594ede2df1d80bea63c7237632bfd80c52cc
mevalba <markearvinalba@gmail.com>
2022-05-20 23:11:18 +0800

    Integrated new functionalities into 'aprgalgorithm'.

---
+++ AprgAlgorithm/AprgAlgorithm/tst/AlgorithmTests/Utilities/MatrixUtilities.cpp
@@ -0,0 +1,31 @@
+#include "MatrixUtilities.hpp"
+
+#include <Common/Math/AlbaMathHelper.hpp>
+
+namespace alba
+{
+
+namespace matrix
+{
+
+template <>
+bool isEqualForMathMatrixDataType(unsigned int const& value1, unsigned int const& value2)
+{
+    return mathHelper::isAlmostEqual(value1, value2);
+}
+
+template <>
+bool isEqualForMathMatrixDataType(int const& value1, int const& value2)
+{
+    return mathHelper::isAlmostEqual(value1, value2);
+}
+
+template <>
+bool isEqualForMathMatrixDataType(double const& value1, double const& value2)
+{
+    return mathHelper::isAlmostEqual(value1, value2);
+}
+
+}
+
+}
