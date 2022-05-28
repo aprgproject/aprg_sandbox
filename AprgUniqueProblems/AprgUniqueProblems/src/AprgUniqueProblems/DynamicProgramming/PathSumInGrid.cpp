e2a12bb82e9e774fc1d7dbff17be373b99b833ec
mevalba <markearvinalba@gmail.com>
2022-05-28 21:47:20 +0800

    Added new features to 'aprguniqueproblems'.

---
+++ AprgUniqueProblems/AprgUniqueProblems/src/AprgUniqueProblems/DynamicProgramming/PathSumInGrid.cpp
@@ -0,0 +1,79 @@
+#include "PathSumInGrid.hpp"
+
+#include <algorithm>
+
+using namespace std;
+
+namespace alba
+{
+
+PathSumInGrid::PathSumInGrid(Grid const& gridToCheck)
+    : m_gridToCheck(gridToCheck)
+    , m_partialSumGrid(gridToCheck)
+{
+    calculatePartialSums();
+}
+
+PathSumInGrid::Value PathSumInGrid::getMaxSumInRightOrDownTraversal()
+{
+    return m_partialSumGrid.getEntry(m_partialSumGrid.getNumberOfColumns()-1, m_partialSumGrid.getNumberOfRows()-1);
+}
+
+PathSumInGrid::Path PathSumInGrid::getMaxPathInRightOrDownTraversal()
+{
+    unsigned int x=m_partialSumGrid.getNumberOfColumns()-1, y=m_partialSumGrid.getNumberOfRows()-1;
+    Path reversePath{m_gridToCheck.getEntry(x, y)};
+    bool isNextXInside(true), isNextYInside(true);
+    while(true)
+    {
+        isNextXInside = m_partialSumGrid.isInside(x-1, y);
+        isNextYInside = m_partialSumGrid.isInside(x, y-1);
+        if(!isNextXInside && !isNextYInside)
+        {
+            break;
+        }
+        else if(!isNextXInside)
+        {
+            reversePath.emplace_back(m_gridToCheck.getEntry(x, y-1));
+            y--;
+        }
+        else if(!isNextYInside)
+        {
+            reversePath.emplace_back(m_gridToCheck.getEntry(x-1, y));
+            x--;
+        }
+        else
+        {
+            if(m_partialSumGrid.getEntry(x-1, y) >= m_partialSumGrid.getEntry(x, y-1))
+            {
+                reversePath.emplace_back(m_gridToCheck.getEntry(x-1, y));
+                x--;
+            }
+            else
+            {
+                reversePath.emplace_back(m_gridToCheck.getEntry(x, y-1));
+                y--;
+            }
+        }
+    }
+    return Path(reversePath.crbegin(), reversePath.crend());
+}
+
+void PathSumInGrid::calculatePartialSums()
+{
+    m_partialSumGrid.iterateAllThroughYAndThenX([&](unsigned int x, unsigned int y)
+    {
+        Value maxNeighbor=0;
+        if(m_partialSumGrid.isInside(x-1, y))
+        {
+            maxNeighbor = max(maxNeighbor, m_partialSumGrid.getEntry(x-1, y));
+        }
+        if(m_partialSumGrid.isInside(x, y-1))
+        {
+            maxNeighbor = max(maxNeighbor, m_partialSumGrid.getEntry(x, y-1));
+        }
+        m_partialSumGrid.getEntryReference(x, y) += maxNeighbor;
+    });
+}
+
+}
---
+++ AprgUniqueProblems/AprgUniqueProblems/src/AprgUniqueProblems/DynamicProgramming/PathSumInGrid.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Common/Math/Matrix/AlbaMatrix.hpp>
+
+#include <vector>
+
+namespace alba
+{
+
+class PathSumInGrid
+{
+public:
+    // Our next problem is to find a path from the upper-left corner to the lower-right corner of an n X n grid, such that we only move down and right.
+    // Each square contains a positive integer, and the path should be constructed so that the sum of the values along the path is as large as possible.
+
+    using Value = unsigned int;
+    using Grid = matrix::AlbaMatrix<Value>;
+    using Path = std::vector<Value>;
+
+    PathSumInGrid(Grid const& gridToCheck);
+
+    Value getMaxSumInRightOrDownTraversal();
+    Path getMaxPathInRightOrDownTraversal();
+private:
+    void calculatePartialSums();
+    Grid m_gridToCheck;
+    Grid m_partialSumGrid;
+};
+
+}
