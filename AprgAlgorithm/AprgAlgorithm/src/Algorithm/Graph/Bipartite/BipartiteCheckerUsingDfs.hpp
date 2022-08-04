9b9ddfe8cece10c7a4d93434b662030f3252c12e
mevalba <markearvinalba@gmail.com>
2022-08-05 01:34:06 +0800

    Introduced enhancements to 'aprgalgorithm'.

--- AprgAlgorithm/AprgAlgorithm/src/Algorithm/Graph/Bipartite/BipartiteCheckerUsingDfs.hpp
+++ AprgAlgorithm/AprgAlgorithm/src/Algorithm/Graph/Bipartite/BipartiteCheckerUsingDfs.hpp
@@ -31,25 +31,40 @@ public:
         return m_isBipartite;
     }
 
-    bool getColor(Vertex const& vertex) const
+    bool hasFirstColor(Vertex const& vertex) const
     {
         bool result(false);
         auto it = m_vertexToColorMap.find(vertex);
         if(it != m_vertexToColorMap.cend())
         {
-            result = it->second;
+            result = !it->second;
         }
         return result;
     }
 
+    void retrieveVerticesWithColor(
+            Vertices & verticesWithFirstColor,
+            Vertices & verticesWithSecondColor) const
+    {
+        for(auto const& vertexColorPair : m_vertexToColorMap)
+        {
+            if(!vertexColorPair.second)
+            {
+                verticesWithFirstColor.emplace_back(vertexColorPair.first);
+            }
+            else
+            {
+                verticesWithSecondColor.emplace_back(vertexColorPair.first);
+            }
+        }
+    }
+
 private:
 
-    bool areColorsEqualOnVertices(Vertex const& vertex1, Vertex const& vertex2) const
-    {
+    bool areColorsEqualOnVertices(Vertex const& vertex1, Vertex const& vertex2) const    {
         bool result(false);
         auto it1 = m_vertexToColorMap.find(vertex1);
-        auto it2 = m_vertexToColorMap.find(vertex2);
-        if(it1 != m_vertexToColorMap.cend() && it2 != m_vertexToColorMap.cend())
+        auto it2 = m_vertexToColorMap.find(vertex2);        if(it1 != m_vertexToColorMap.cend() && it2 != m_vertexToColorMap.cend())
         {
             result = it1->second == it2->second;
         }
--- AprgAlgorithm/AprgAlgorithm/tst/AlgorithmTests/Graph/Bipartite/MaximumMatchings_unit.cpp
+++ AprgAlgorithm/AprgAlgorithm/tst/AlgorithmTests/Graph/Bipartite/MaximumMatchings_unit.cpp
@@ -29,13 +29,26 @@ TEST(MaximumMatchingsTest, GeMaximumMatchingsWorksOnExample1)
     graph.connect(3U, 6U);
     graph.connect(3U, 8U);
     graph.connect(4U, 7U);
-    MaximumMatchingsForTest maximumMatchings;
+    MaximumMatchingsForTest maximumMatchings(graph);
 
     Edges expectedMatchings{{1U, 5U}, {2U, 7U}, {3U, 6U}};
+    EXPECT_EQ(expectedMatchings, maximumMatchings.getMaximumMatchings(newSourceVertex, newSinkVertex));
+}
 
-    EXPECT_EQ(expectedMatchings, maximumMatchings.getMaximumMatchings(graph, newSourceVertex, newSinkVertex));
+TEST(MaximumMatchingsTest, DoesHavePerfectMatchingByCheckingASubsetWorksOnExample1)
+{
+    GraphForTest graph;
+    graph.connect(1U, 5U);
+    graph.connect(2U, 7U);
+    graph.connect(3U, 5U);
+    graph.connect(3U, 6U);
+    graph.connect(3U, 8U);
+    graph.connect(4U, 7U);
+    MaximumMatchingsForTest maximumMatchings(graph);
+
+    EXPECT_TRUE(maximumMatchings.doesHavePerfectMatchingByCheckingASubset({1U, 3U}));
+    EXPECT_FALSE(maximumMatchings.doesHavePerfectMatchingByCheckingASubset({2U, 4U}));
 }
 
 }
-
 }
