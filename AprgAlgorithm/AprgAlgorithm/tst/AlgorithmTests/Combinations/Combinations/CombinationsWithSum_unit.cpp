a72b32097ab2f8e32572fb4747142cd125f5214b
mevalba <markearvinalba@gmail.com>
2022-08-27 12:11:48 +0800

    Added advanced functionalities to 'aprgalgorithm'.

--- AprgAlgorithm/AprgAlgorithm/tst/AlgorithmTests/Combinations/Combinations/CombinationsWithSum_unit.cpp
+++ AprgAlgorithm/AprgAlgorithm/tst/AlgorithmTests/Combinations/Combinations/CombinationsWithSum_unit.cpp
@@ -17,26 +17,23 @@ using GenerationForTest = CombinationsWithSum<Values>;
 using Combinations = GenerationForTest::Combinations;
 }
 
-TEST(CombinationsWithSumUsingRecursionTest, GetCombinationsWithSumUsingRecursionWorks)
+TEST(CombinationsWithSumTest, GetCombinationsWithSumUsingRecursionWorks)
 {
     Values inputValues{1U, 2U, 3U, 4U};
     GenerationForTest generation(inputValues);
-
     Combinations actualCombinations(generation.getCombinationsWithSumUsingRecursion(4U));
 
     Combinations expectedCombinations{{1U, 1U, 1U, 1U}, {1U, 1U, 2U}, {1U, 3U}, {2U, 2U}, {4U}};
     EXPECT_EQ(expectedCombinations, actualCombinations);
 }
 
-TEST(CombinationsWithSumUsingRecursionTest, GetCombinationsWithSumUsingLoopsWorks)
+TEST(CombinationsWithSumTest, GetCombinationsWithSumUsingLoopsWorks)
 {
     Values inputValues{1U, 2U, 3U, 4U};
     GenerationForTest generation(inputValues);
-
     Combinations actualCombinations(generation.getCombinationsWithSumUsingLoops(4U));
 
-    Combinations expectedCombinations{{1U, 1U, 1U, 1U}, {1U, 1U, 2U}, {1U, 3U}, {2U, 2U}, {4U}};
-    EXPECT_EQ(expectedCombinations, actualCombinations);
+    Combinations expectedCombinations{{1U, 1U, 1U, 1U}, {1U, 1U, 2U}, {1U, 3U}, {2U, 2U}, {4U}};    EXPECT_EQ(expectedCombinations, actualCombinations);
 }
 
 }
