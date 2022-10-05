58e3082bf3165d95b8d87938307b66f31f15e8d2
mevalba <markearvinalba@gmail.com>
2022-10-06 00:34:06 +0800

    Added valuable functionalities to 'aprgmath' module.

--- AprgMath/AprgMath/tst/Math/GameTheory/SpecificGames/GameWithMaze_unit.cpp
+++ AprgMath/AprgMath/tst/Math/GameTheory/SpecificGames/GameWithMaze_unit.cpp
@@ -24,10 +24,34 @@ TEST(GameWithMazeTest, Example1Works)
      false, false, false, false, false});
     GameWithMaze gameWithMaze(isBlockedMatrix);
 
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({0U, 0U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({0U, 1U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({0U, 2U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({0U, 3U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({0U, 4U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({1U, 0U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({1U, 1U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({1U, 2U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({1U, 3U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({1U, 4U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({2U, 0U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({2U, 1U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({2U, 2U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({2U, 3U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({2U, 4U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({3U, 0U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({3U, 1U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({3U, 2U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({3U, 3U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({3U, 4U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({4U, 0U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({4U, 1U}));
+    EXPECT_EQ(GameState::Losing, gameWithMaze.getGameStateAt({4U, 2U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({4U, 3U}));
+    EXPECT_EQ(GameState::Winning, gameWithMaze.getGameStateAt({4U, 4U}));
     EXPECT_EQ(3U, gameWithMaze.getGrundyNumberAt({4U, 4U}));
     ALBA_PRINT1(gameWithMaze.getString());
 }
-
 }
 
 }
