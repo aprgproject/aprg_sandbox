3678f45f2cb13875ded5e2e461cb3738d33f41bb
mevalba <markearvinalba@gmail.com>
2022-11-06 14:39:22 +0800

    Refactor aprgbitmap module

--- AprgBitmap/AprgBitmap/src/Bitmap/CommonTypes.hpp
+++ AprgBitmap/AprgBitmap/src/Bitmap/CommonTypes.hpp
@@ -15,9 +15,9 @@ using BitmapSignedXY = AlbaXY<int>;
 using BitmapXY = AlbaXY<unsigned int>;
 using BitmapDoubleXY = AlbaXY<double>;
 using BitmapXYs = std::vector<BitmapXY>;
+using BitmapSignedXYs = std::vector<BitmapSignedXY>;
 using Colors = std::vector<uint32_t>;
 using PixelData = AlbaMemoryBuffer;
-
 }
 
 }
