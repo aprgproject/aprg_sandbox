4da0f89cc5a677d4117b74cdf9056e4447fcaf91
mevalba <markearvinalba@gmail.com>
2022-11-16 23:30:10 +0800

    Refactor aprgalgebra module

--- AprgAlgebra/AprgAlgebra/tst/AlgebraTests/Series/SpecificSeries/TaylorSeries_unit.cpp
+++ AprgAlgebra/AprgAlgebra/tst/AlgebraTests/Series/SpecificSeries/TaylorSeries_unit.cpp
@@ -29,19 +29,10 @@ TEST(TaylorSeriesTest, GetFormulaForTaylorSeriesWorks)
 
 TEST(TaylorSeriesTest, GetValueAtIndexWorks)
 {
-    Term formula(createExpressionIfPossible({getEAsATerm(), "^", "x"}));
-    TaylorSeries series(formula, "x");
+    // taylors formula: f(b) = f(a) + f'(a)*(b-a)/1! + f''(a)*(b-a)^2/2! ...
+    // Given that "starting value" is 0 and "value to approach" is x, the formula for e^x becomes:
+    // e^x = 1 + x + x^2/2! + x^3/3! + x^4/4! + x^5/5! ...
 
-    string stringToExpect1("1");
-    string stringToExpect2("(e)[x]");
-    string stringToExpect3("(3.694528049465325[x^2] + -7.38905609893065[x] + 7.38905609893065)");
-    EXPECT_EQ(stringToExpect1, series.getValueAtIndex(0, 0).getDisplayableString());
-    EXPECT_EQ(stringToExpect2, series.getValueAtIndex(1, 1).getDisplayableString());
-    EXPECT_EQ(stringToExpect3, series.getValueAtIndex(2, 2).getDisplayableString());
-}
-
-TEST(TaylorSeriesTest, GetValueForMaclaurinSeriesAtIndexWorks)
-{
     Term formula(createExpressionIfPossible({getEAsATerm(), "^", "x"}));
     TaylorSeries series(formula, "x");
 
@@ -49,12 +40,28 @@ TEST(TaylorSeriesTest, GetValueForMaclaurinSeriesAtIndexWorks)
     string stringToExpect2("(1[x] + 1)");
     string stringToExpect3("((1/2)[x^2] + 1[x] + 1)");
     string stringToExpect4("((1/6)[x^3] + (1/2)[x^2] + 1[x] + 1)");
+    string stringToExpect5("((1/24)[x^4] + (1/6)[x^3] + (1/2)[x^2] + 1[x] + 1)");
+    EXPECT_EQ(stringToExpect1, series.getValueAtIndex(0, 0).getDisplayableString());
+    EXPECT_EQ(stringToExpect2, series.getValueAtIndex(1, 0).getDisplayableString());
+    EXPECT_EQ(stringToExpect3, series.getValueAtIndex(2, 0).getDisplayableString());
+    EXPECT_EQ(stringToExpect4, series.getValueAtIndex(3, 0).getDisplayableString());
+    EXPECT_EQ(stringToExpect5, series.getValueAtIndex(4, 0).getDisplayableString());
+}
+
+TEST(TaylorSeriesTest, GetValueForMaclaurinSeriesAtIndexWorks){
+    Term formula(createExpressionIfPossible({getEAsATerm(), "^", "x"}));
+    TaylorSeries series(formula, "x");
+    string stringToExpect1("1");
+    string stringToExpect2("(1[x] + 1)");
+    string stringToExpect3("((1/2)[x^2] + 1[x] + 1)");
+    string stringToExpect4("((1/6)[x^3] + (1/2)[x^2] + 1[x] + 1)");
+    string stringToExpect5("((1/24)[x^4] + (1/6)[x^3] + (1/2)[x^2] + 1[x] + 1)");
     EXPECT_EQ(stringToExpect1, series.getValueForMaclaurinSeriesAtIndex(0).getDisplayableString());
     EXPECT_EQ(stringToExpect2, series.getValueForMaclaurinSeriesAtIndex(1).getDisplayableString());
     EXPECT_EQ(stringToExpect3, series.getValueForMaclaurinSeriesAtIndex(2).getDisplayableString());
     EXPECT_EQ(stringToExpect4, series.getValueForMaclaurinSeriesAtIndex(3).getDisplayableString());
+    EXPECT_EQ(stringToExpect5, series.getValueForMaclaurinSeriesAtIndex(4).getDisplayableString());
 }
 
 }
-
 }
