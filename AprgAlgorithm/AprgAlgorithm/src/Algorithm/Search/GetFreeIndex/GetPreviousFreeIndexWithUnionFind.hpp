d03724cb6260623a7b3db4d99e1e858ea8766009
mevalba <markearvinalba@gmail.com>
2022-12-10 00:40:55 +0800

    Integrate union find classes into APRG algorithm

---
+++ AprgAlgorithm/AprgAlgorithm/src/Algorithm/Search/GetFreeIndex/GetPreviousFreeIndexWithUnionFind.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <Algorithm/Search/GetFreeIndex/Common/GetFreeIndexWithUnionFind.hpp>
+
+namespace alba
+{
+
+namespace algorithm
+{
+
+template <typename Index>
+class GetPreviousFreeIndexWithUnionFind : public GetFreeIndexWithUnionFind<Index>
+{
+public:
+    using BaseClass = GetFreeIndexWithUnionFind<Index>;
+    using UnionFind = typename BaseClass::UnionFind;
+
+    GetPreviousFreeIndexWithUnionFind(Index const numberOfIndexes)
+        : BaseClass(numberOfIndexes)
+        , b_numberOfIndexes(BaseClass::m_numberOfIndexes)
+        , b_unionFind(BaseClass::m_unionFind)
+    {}
+
+    Index getPreviousFreeIndexAt(Index const index) const
+    {
+        // This has log(N) running time
+
+        return BaseClass::getFreeIndexAt(index);
+    }
+
+    void setAsNotFree(Index const index)
+    {
+        if(index>0 && index<b_numberOfIndexes)
+        {
+            b_unionFind.connect(b_unionFind.getRoot(index-1), index);
+        }
+    }
+
+private:
+    Index & b_numberOfIndexes;
+    UnionFind & b_unionFind;
+};
+
+}
+
+}
