46b89f33aad96208e4b1eb32ce446f6b708e3483
mevalba <markearvinalba@gmail.com>
2023-01-26 02:52:27 +0800

    Augmented problem variety in APRG unique problems

--- AprgUniqueProblems/AprgUniqueProblems/tst/AprgUniqueProblemsTests/DynamicProgramming/Other/ProductDayProblem_unit.cpp
+++ AprgUniqueProblems/AprgUniqueProblems/tst/AprgUniqueProblemsTests/DynamicProgramming/Other/ProductDayProblem_unit.cpp
@@ -12,15 +12,15 @@ namespace
 using PriceMatrix=ProductDayProblem::PriceMatrix;
 }
 
-TEST(ProductDayProblemTest, GetProductAndDayPairsForMinimumPriceWorksOnExample1)
+TEST(ProductDayProblemTest, GetMinimumPriceWorksOnExample1)
 {
     PriceMatrix pricesInDayByProduct(8U, 3U,
-    {6U, 9U, 5U, 2U, 8U, 9U, 1U, 6U,
-     8U, 2U, 6U, 2U, 7U, 5U, 7U, 2U,
+    {6U, 9U, 5U, 2U, 8U, 9U, 1U, 6U,     8U, 2U, 6U, 2U, 7U, 5U, 7U, 2U,
      5U, 3U, 9U, 7U, 3U, 5U, 1U, 4U});
     ProductDayProblem queryForTest(pricesInDayByProduct);
 
-    EXPECT_EQ(5U, queryForTest.getProductAndDayPairsForMinimumPrice());
+    EXPECT_EQ(5U, queryForTest.getMinimumPriceUsingMemoizationDP());
+    EXPECT_EQ(5U, queryForTest.getMinimumPriceUsingTabularDP());
 }
 
 }
