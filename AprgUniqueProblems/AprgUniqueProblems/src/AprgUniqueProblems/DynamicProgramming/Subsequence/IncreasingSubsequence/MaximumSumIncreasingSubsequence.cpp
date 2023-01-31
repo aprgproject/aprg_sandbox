59c919c0fb294a728113dbeee3952584f6524c5c
mevalba <markearvinalba@gmail.com>
2023-01-31 10:43:01 +0800

    Expanded problem set in APRG unique problems

--- AprgUniqueProblems/AprgUniqueProblems/src/AprgUniqueProblems/DynamicProgramming/Subsequence/IncreasingSubsequence/MaximumSumIncreasingSubsequence.cpp
+++ AprgUniqueProblems/AprgUniqueProblems/src/AprgUniqueProblems/DynamicProgramming/Subsequence/IncreasingSubsequence/MaximumSumIncreasingSubsequence.cpp
@@ -65,15 +65,13 @@ MaximumSumIncreasingSubsequence::Values MaximumSumIncreasingSubsequence::getSubs
 
         // construct longest sequence
         auto itMax = max_element(partialSums.cbegin(), partialSums.cend());
-        Index indexOfMaxSum = distance(partialSums.cbegin(), itMax );
+        Index indexOfMaxSum = distance(partialSums.cbegin(), itMax);
         Index traverseIndex=indexOfMaxSum;
         for(; traverseIndex!=indexToPreviousIndex.at(traverseIndex); traverseIndex=indexToPreviousIndex.at(traverseIndex))
-        {
-            result.emplace_back(m_sequence.at(traverseIndex));
+        {            result.emplace_back(m_sequence.at(traverseIndex));
         }
         result.emplace_back(m_sequence.at(traverseIndex));
-        reverse(result.begin(), result.end());
-    }
+        reverse(result.begin(), result.end());    }
     return result;
 }
 
