e647c0ca334e7859197416148a3ad1b11e1e9273
mevalba <markearvinalba@gmail.com>
2023-02-03 00:08:48 +0800

    Incorporate additional functionalities into aprgalgorithm module

--- AprgAlgorithm/AprgAlgorithm/tst/AlgorithmTests/Graph/Tree/AncestorsInTree_unit.cpp
+++ AprgAlgorithm/AprgAlgorithm/tst/AlgorithmTests/Graph/Tree/AncestorsInTree_unit.cpp
@@ -13,27 +13,24 @@ namespace
 {
 using VertexForTest = unsigned int;
 using GraphForTest = UndirectedGraphWithListOfEdges<VertexForTest>;
-using AncestorsForTest = AncestorsInTree<VertexForTest>;
+using QueryForTest = AncestorsInTree<VertexForTest>;
 }
 
-TEST(AncestorsInTreeTest, GetAncestorWorks)
-{
+TEST(AncestorsInTreeTest, GetAncestorWorks){
     GraphForTest graph;
     graph.connect(1U, 2U);
-    graph.connect(1U, 4U);
-    graph.connect(1U, 5U);
+    graph.connect(1U, 4U);    graph.connect(1U, 5U);
     graph.connect(2U, 6U);
     graph.connect(4U, 3U);
     graph.connect(4U, 7U);
     graph.connect(7U, 8U);
-    AncestorsForTest ancestors(graph, 1U);
+    QueryForTest query(graph, 1U);
 
-    EXPECT_EQ(1U, ancestors.getAncestor(2U, 1U));
-    EXPECT_EQ(7U, ancestors.getAncestor(8U, 1U));
-    EXPECT_EQ(4U, ancestors.getAncestor(8U, 2U));
-    EXPECT_EQ(1U, ancestors.getAncestor(8U, 3U));
+    EXPECT_EQ(1U, query.getAncestor(2U, 1U));
+    EXPECT_EQ(7U, query.getAncestor(8U, 1U));
+    EXPECT_EQ(4U, query.getAncestor(8U, 2U));
+    EXPECT_EQ(1U, query.getAncestor(8U, 3U));
 }
 
 }
-
 }
---
+++ AprgAlgorithm/AprgAlgorithm/tst/AlgorithmTests/Graph/Tree/ChildrenInTree_unit.cpp
@@ -0,0 +1,40 @@
+#include <Algorithm/Graph/Tree/ChildrenInTree.hpp>
+#include <Algorithm/Graph/UndirectedGraph/UndirectedGraphWithListOfEdges.hpp>
+
+#include <gtest/gtest.h>
+
+namespace alba
+{
+
+namespace algorithm
+{
+
+namespace
+{
+using VertexForTest = unsigned int;
+using GraphForTest = UndirectedGraphWithListOfEdges<VertexForTest>;
+using QueryForTest = ChildrenInTree<VertexForTest>;
+using VerticesForTest = QueryForTest::Vertices;
+}
+
+TEST(ChildrenInTreeTest, GetChildrenWorks)
+{
+    GraphForTest graph;
+    graph.connect(1U, 2U);
+    graph.connect(1U, 4U);
+    graph.connect(1U, 5U);
+    graph.connect(2U, 6U);
+    graph.connect(4U, 3U);
+    graph.connect(4U, 7U);
+    graph.connect(7U, 8U);
+    QueryForTest query(graph, 1U);
+
+    EXPECT_EQ((VerticesForTest{2U, 4U, 5U}), query.getChildren(1U));
+    EXPECT_EQ((VerticesForTest{6U}), query.getChildren(2U));
+    EXPECT_EQ((VerticesForTest{3U, 7U}), query.getChildren(4U));
+    EXPECT_EQ((VerticesForTest{8U}), query.getChildren(7U));
+}
+
+}
+
+}
