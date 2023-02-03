96f8e4fda92417e49480f165a5a3e66eaa2f87fc
mevalba <markearvinalba@gmail.com>
2023-02-03 22:50:14 +0800

    Augment problem variety in APRG unique problems

--- AprgUniqueProblems/AprgUniqueProblems/src/AprgUniqueProblems/DynamicProgramming/Other/MaximumSubMatrixSum.cpp
+++ AprgUniqueProblems/AprgUniqueProblems/src/AprgUniqueProblems/DynamicProgramming/Other/MaximumSubMatrixSum.cpp
@@ -16,48 +16,45 @@ MaximumSubMatrixSum::Value MaximumSubMatrixSum::getMaximumSubMatrixSum() const
 {
     // Uses Kadane’s algorithm
 
-    // Time Complexity: O(n)
-    // Auxiliary Space: O(1)
+    // Time Complexity: O(n^3)
+    // Auxiliary Space: O(n)
 
     Value result = MIN_VALUE;
-    for (Index left = 0; left < m_valueMatrix.getNumberOfColumns(); left++)
-    {
+    for (Index left = 0; left < m_valueMatrix.getNumberOfColumns(); left++)    {
         Values accumulatedColumn;
         m_valueMatrix.retrieveColumn(accumulatedColumn, left);
         for (Index right = left+1; right < m_valueMatrix.getNumberOfColumns(); right++)
         {
             MaximumSubArraySum maximumSubArraySum(accumulatedColumn);
-            result = max(result, maximumSubArraySum.getMaximumSubArraySum());
+            result = max(result, maximumSubArraySum.getMaximumSubArraySum()); // linear
 
             for (Index rowIndex = 0; rowIndex < m_valueMatrix.getNumberOfRows(); rowIndex++) // add next column
-            {
-                accumulatedColumn[rowIndex] += m_valueMatrix.getEntry(right, rowIndex);
+            {                accumulatedColumn[rowIndex] += m_valueMatrix.getEntry(right, rowIndex);
             }
         }
-    }
-    return result;
+    }    return result;
 }
 
 MaximumSubMatrixSum::SubArrayDetails MaximumSubMatrixSum::getMaximumSubMatrixSumWithDetails() const
 {
+    // Time Complexity: O(n^3)
+    // Auxiliary Space: O(n)
+
     SubArrayDetails result{0U, 0U, 0U, 0U, MIN_VALUE};
     for (Index left = 0; left < m_valueMatrix.getNumberOfColumns(); left++)
-    {
-        Values accumulatedColumn;
+    {        Values accumulatedColumn;
         m_valueMatrix.retrieveColumn(accumulatedColumn, left);
         for (Index right = left+1; right < m_valueMatrix.getNumberOfColumns(); right++)
         {
             MaximumSubArraySum maximumSubArraySum(accumulatedColumn);
 
-            auto columnSumDetails(maximumSubArraySum.getMaximumSubArraySumWithDetails());
+            auto columnSumDetails(maximumSubArraySum.getMaximumSubArraySumWithDetails()); // linear
             if(result.sum < columnSumDetails.sum)
             {
-                result.sum = columnSumDetails.sum;
-                result.left = left;
+                result.sum = columnSumDetails.sum;                result.left = left;
                 result.right = right;
                 result.up = columnSumDetails.lowIndex;
-                result.down = columnSumDetails.highIndex;
-            }
+                result.down = columnSumDetails.highIndex;            }
 
             for (Index rowIndex = 0; rowIndex < m_valueMatrix.getNumberOfRows(); rowIndex++) // add next column
             {
