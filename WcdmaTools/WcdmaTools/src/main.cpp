0bf341abba9fb4d0da097db98737864f2afecf38
mevalba <markearvinalba@gmail.com>
2023-03-04 16:44:40 +0800

    Enhanced code quality in "wcdmatools"

--- WcdmaTools/WcdmaTools/src/main.cpp
+++ WcdmaTools/WcdmaTools/src/main.cpp
@@ -11,15 +11,13 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout<<"WcdmaTools created by APRG"<<endl;
-    cout<<"APRG_CODE_VERSION: "<<APRG_CODE_VERSION<<endl;
+    cout<<"WcdmaTools created by APRG\n";
+    cout<<"APRG_CODE_VERSION: "<<APRG_CODE_VERSION<<"\n";
     QApplication a(argc, argv);
     WcdmaTools w;
-    if(argc==2)
-    {
+    if(argc==2)    {
         w.setInputFileOrDirectory(alba::stringHelper::getStringWithoutCharAtTheEnd(string(argv[1]), '"'));
     }
     w.show();
-
     return a.exec();
 }
