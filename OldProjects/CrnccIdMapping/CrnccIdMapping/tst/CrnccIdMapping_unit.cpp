91c578d01e3d74561524381a6e69e69ffae59979
mevalba <markearvinalba@gmail.com>
2023-03-09 09:45:21 +0800

    Improved design in "crnccid"

--- OldProjects/CrnccIdMapping/CrnccIdMapping/tst/CrnccIdMapping_unit.cpp
+++ OldProjects/CrnccIdMapping/CrnccIdMapping/tst/CrnccIdMapping_unit.cpp
@@ -1,49 +1,44 @@
-#include <Common/Randomizer/AlbaRandomizer.hpp>
+#include <Common/Randomizer/AlbaSimpleRandomizer.hpp>
 #include <CrnccIdMapping.hpp>
 
 #include <gtest/gtest.h>
-
 using namespace std;
 
-namespace alba
-{
+namespace alba{
 
 TEST(SampleTest, DISABLED_PerformanceAccessTestWithRandomValues)
 {
     constexpr unsigned int initialSize=2500;
     constexpr unsigned int accessIterations=1000000;
 
-    AlbaRandomizer randomizer;
+    AlbaSimpleRandomizer randomizer;
     vector<unsigned int> crnccIds;
     for(unsigned int currentSize=0; currentSize<initialSize; currentSize++)
     {
-        TCRNCCommunicationContextId crnccId = randomizer.getRandomValueInUniformDistribution(1,65536);
-        TNbccId nbccId = randomizer.getRandomValueInUniformDistribution(1,2500);
+        TCRNCCommunicationContextId crnccId = randomizer.getRandomIntegerInUniformDistribution(1,65536);
+        TNbccId nbccId = randomizer.getRandomIntegerInUniformDistribution(1,2500);
         crnccIds.emplace_back(crnccId);
         setCrnccIdMapping(crnccId, nbccId);
     }
 
-
     for(unsigned int iteration=0; iteration<accessIterations; iteration++)
     {
-        TCRNCCommunicationContextId crnccIdAccess = crnccIds[randomizer.getRandomValueInUniformDistribution(0, crnccIds.size()-1)];
+        TCRNCCommunicationContextId crnccIdAccess = crnccIds[randomizer.getRandomIntegerInUniformDistribution(0, crnccIds.size()-1)];
         TNbccId nbccIdAccess = getNbccIdFromCrnccId(crnccIdAccess);
     }
 }
-
 TEST(SampleTest, DISABLED_PerformanceAddTestWithRandomValues)
 {
     constexpr unsigned int initialSize=1000000;
 
-    AlbaRandomizer randomizer;
+    AlbaSimpleRandomizer randomizer;
     vector<unsigned int> crnccIds;
     for(unsigned int currentSize=0; currentSize<initialSize; currentSize++)
     {
-        TCRNCCommunicationContextId crnccId = randomizer.getRandomValueInUniformDistribution(1,65536);
-        TNbccId nbccId = randomizer.getRandomValueInUniformDistribution(1,2500);
+        TCRNCCommunicationContextId crnccId = randomizer.getRandomIntegerInUniformDistribution(1,65536);
+        TNbccId nbccId = randomizer.getRandomIntegerInUniformDistribution(1,2500);
         crnccIds.emplace_back(crnccId);
         setCrnccIdMapping(crnccId, nbccId);
-    }
-}
+    }}
 
 }
