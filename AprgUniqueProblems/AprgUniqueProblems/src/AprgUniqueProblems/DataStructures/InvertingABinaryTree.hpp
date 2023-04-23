2cf3c094576585171e27bd5e1c2c175e4ee1f385
mevalba <markearvinalba@gmail.com>
2023-04-23 18:25:55 +0800

    Revamped aprguniqueproblems module

--- AprgUniqueProblems/AprgUniqueProblems/src/AprgUniqueProblems/DataStructures/InvertingABinaryTree.hpp
+++ AprgUniqueProblems/AprgUniqueProblems/src/AprgUniqueProblems/DataStructures/InvertingABinaryTree.hpp
@@ -13,11 +13,10 @@ struct Node
     unsigned int value;
     std::unique_ptr<Node> leftPointer;
     std::unique_ptr<Node> rightPointer;
+    // ~Node(); blows up the stack
 };
 
 std::unique_ptr<Node> invertABinaryTree(std::unique_ptr<Node> parent);
-
 }
-
 
 }
