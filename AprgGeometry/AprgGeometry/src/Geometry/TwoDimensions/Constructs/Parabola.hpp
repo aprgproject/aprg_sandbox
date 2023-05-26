56f8508f979c1c62d692abe7eb2de42411550705
mevalba <markearvinalba@gmail.com>
2023-05-26 18:29:35 +0800

    Rectified issues in aprggeometry module

--- AprgGeometry/AprgGeometry/src/Geometry/TwoDimensions/Constructs/Parabola.hpp
+++ AprgGeometry/AprgGeometry/src/Geometry/TwoDimensions/Constructs/Parabola.hpp
@@ -16,14 +16,13 @@ enum class ParabolaOrientation {
 
 template <ParabolaOrientation orientation>
 struct ParabolaOrientationTraits {
-    static_assert(sizeof(orientation) == -1, "Parabola orientation traits are not defined");
+    // causes compilation error
+    // static_assert(sizeof(orientation) == -1, "Parabola orientation traits are not defined");
 };
 
-template <>
-struct ParabolaOrientationTraits<ParabolaOrientation::PolynomialX> {
+template <>struct ParabolaOrientationTraits<ParabolaOrientation::PolynomialX> {
     using ParabolaOrientationPolynomialType = PolynomialInXEqualsY<3>;
 };
-
 template <>
 struct ParabolaOrientationTraits<ParabolaOrientation::PolynomialY> {
     using ParabolaOrientationPolynomialType = PolynomialInYEqualsX<3>;
