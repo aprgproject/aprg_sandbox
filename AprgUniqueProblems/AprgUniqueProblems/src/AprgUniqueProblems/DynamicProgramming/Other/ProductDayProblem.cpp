4aa5812cc2a5ddbd7b405d045f1cc6eff58538be
mevalba <markearvinalba@gmail.com>
2023-05-26 18:34:27 +0800

    Resolved problems in aprguniqueproblems module

--- AprgUniqueProblems/AprgUniqueProblems/src/AprgUniqueProblems/DynamicProgramming/Other/ProductDayProblem.cpp
+++ AprgUniqueProblems/AprgUniqueProblems/src/AprgUniqueProblems/DynamicProgramming/Other/ProductDayProblem.cpp
@@ -74,15 +74,13 @@ ProductDayProblem::Price ProductDayProblem::getMinimumPriceUsingIterativeDP() co
 
 ProductDayProblem::Price ProductDayProblem::getMinimumPriceUsingMemoizationDP(
     PriceMatrix& minimumPrices, Day const day, ProductBits const productBits) const {
-    if (day < getNumberOfDays()) {
+    if (day >= 0 && day < getNumberOfDays()) {
         Price result(minimumPrices.getEntry(day, productBits));
         if (UNUSED_PRICE == result) {
-            // put total of previous day
-            result = getMinimumPriceUsingMemoizationDP(minimumPrices, day - 1, productBits);
+            // put total of previous day            result = getMinimumPriceUsingMemoizationDP(minimumPrices, day - 1, productBits);
             for (Product product = 0; product < getNumberOfProducts(); product++) {
                 if (isProductIncluded(productBits, product)) {
-                    Price previousDayWithoutProduct =                        getMinimumPriceUsingMemoizationDP(minimumPrices, day - 1, removeProduct(productBits, product));
-                    if (INVALID_PRICE != previousDayWithoutProduct) {
+                    Price previousDayWithoutProduct =                        getMinimumPriceUsingMemoizationDP(minimumPrices, day - 1, removeProduct(productBits, product));                    if (INVALID_PRICE != previousDayWithoutProduct) {
                         result = min(
                             result,                    // current value                            previousDayWithoutProduct  // get total of previous day without the product
                                 + m_pricesInDayByProduct.getEntry(day, product));  // plus price of the product today
