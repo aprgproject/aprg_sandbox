6023a624d6e345a0d1f0aaae2f960d4a5815b5e8
mevalba <markearvinalba@gmail.com>
2023-07-05 11:16:44 +0800

    Refine APRG common module

--- AprgCommon/AprgCommon/src/Common/Math/Number/AlbaNumber.hpp
+++ AprgCommon/AprgCommon/src/Common/Math/Number/AlbaNumber.hpp
@@ -13,36 +13,36 @@ namespace alba {
 class AlbaNumber  // This is value type.
 {
 public:
+    using IntDataType = int64_t;
+    using NumeratorDataType = int32_t;
+    using DenominatorDataType = uint32_t;
     using ComplexFloat = AlbaComplexNumber<float>;
     static constexpr double ADJUSTMENT_FLOAT_TOLERANCE = 1E-15;
 
     enum class Type { Integer, Double, Fraction, ComplexNumber };
     struct FractionData  // alignas(8) has no effect on performance (tested in benchmark)
     {
-        int32_t numerator;
-        uint32_t denominator;
+        NumeratorDataType numerator;
+        DenominatorDataType denominator;
     };
     struct ComplexNumberData  // alignas(8) has no effect on performance (tested in benchmark)
-    {
-        float realPart;
+    {        float realPart;
         float imaginaryPart;
     };
     union NumberUnionData  // alignas(8) has no effect on performance (tested in benchmark)
     {
         constexpr NumberUnionData() : intData{} {}
-        constexpr NumberUnionData(int64_t const integer) : intData(integer) {}
+        constexpr NumberUnionData(IntDataType const integer) : intData(integer) {}
         constexpr NumberUnionData(double const doubleValue) : doubleData(doubleValue) {}
         constexpr NumberUnionData(FractionData const& fractionData) : fractionData(fractionData) {}
         constexpr NumberUnionData(ComplexNumberData const& complexNumberData) : complexNumberData(complexNumberData) {}
-        int64_t intData;
+        IntDataType intData;
         double doubleData;
         FractionData fractionData;
-        ComplexNumberData complexNumberData;
-    };
+        ComplexNumberData complexNumberData;    };
 
     struct ConfigurationDetails {
-        double comparisonTolerance;
-        double floatAdjustmentTolerance;
+        double comparisonTolerance;        double floatAdjustmentTolerance;
     };
     class Configuration : public AlbaConfigurationHolder<ConfigurationDetails> {
     public:
@@ -61,16 +61,14 @@ public:
     // static functions
 
     static AlbaNumber createNumberFromDoubleAndRoundIfNeeded(double const doubleValue);
-    static AlbaNumber createFraction(int32_t const numerator, int32_t const denominator);
-    static AlbaNumber createFraction(int32_t const numerator, uint32_t const denominator);
+    static AlbaNumber createFraction(NumeratorDataType const numerator, NumeratorDataType const denominator);
+    static AlbaNumber createFraction(NumeratorDataType const numerator, DenominatorDataType const denominator);
     template <typename NumberType>
     static AlbaNumber createComplexNumber(NumberType const realPart, NumberType const imaginaryPart);
     static AlbaNumber createComplexNumber(ComplexFloat const& complexNumber);
-
     // constexpr functions
 
-    template <typename ArithmeticType>
-    void constexpr checkArithmeticType() {
+    template <typename ArithmeticType>    void constexpr checkArithmeticType() {
         static_assert(sizeof(ArithmeticType) <= 8, "Maximum size is 8 bytes/64 bits.");
         static_assert(
             !(sizeof(ArithmeticType) == 8 && typeHelper::isUnsignedType<ArithmeticType>()),
@@ -92,29 +90,25 @@ public:
             std::enable_if_t<typeHelper::isArithmeticType<ArithmeticType>()>>  // enabled via a type template parameter
     constexpr AlbaNumber(ArithmeticType const value)
         : m_type(getTypeBasedFromArithmeticType<ArithmeticType>()),
-          m_data(
-              static_cast<typeHelper::ConditionalType<typeHelper::isIntegralType<ArithmeticType>(), int64_t, double>>(
-                  value)) {
+          m_data(static_cast<
+                 typeHelper::ConditionalType<typeHelper::isIntegralType<ArithmeticType>(), IntDataType, double>>(
+              value)) {
         checkArithmeticType<ArithmeticType>();
     }
-
     constexpr AlbaNumber(FractionData const& fractionData) : m_type(Type::Fraction), m_data(fractionData) {}
 
-    constexpr AlbaNumber(ComplexNumberData const& complexNumberData)
-        : m_type(Type::ComplexNumber), m_data(complexNumberData) {}
+    constexpr AlbaNumber(ComplexNumberData const& complexNumberData)        : m_type(Type::ComplexNumber), m_data(complexNumberData) {}
 
     // rule of zero
 
-    AlbaNumber(char const character) =
-        delete;  // remove character to integer conversion (delete any functions is a C++11 feature)
+    // remove character to integer conversion (delete any functions is a C++11 feature)
+    AlbaNumber(char const character) = delete;
 
     // This should be constexpr as well but a lot of coding is needed
-    bool operator==(AlbaNumber const& second) const;
-    bool operator!=(AlbaNumber const& second) const;
+    bool operator==(AlbaNumber const& second) const;    bool operator!=(AlbaNumber const& second) const;
     bool operator<=(AlbaNumber const& second) const;
     bool operator>=(AlbaNumber const& second) const;
-    bool operator<(AlbaNumber const& second) const;
-    bool operator>(AlbaNumber const& second) const;
+    bool operator<(AlbaNumber const& second) const;    bool operator>(AlbaNumber const& second) const;
     AlbaNumber operator+() const;
     AlbaNumber operator-() const;
     AlbaNumber operator+(AlbaNumber const& second) const;
@@ -140,14 +134,12 @@ public:
     bool isARealFiniteValue() const;
 
     Type getType() const;
-    int64_t getInteger() const;
+    IntDataType getInteger() const;
     double getDouble() const;
     FractionData getFractionData() const;
     ComplexNumberData getComplexNumberData() const;
-
     void convertToInteger();
     void convertToFraction();
-
 private:
     // static functions
     static double getComparisonTolerance();
@@ -159,38 +151,38 @@ private:
     template <typename NumberType1, typename NumberType2>
     void constructBasedFromComplexNumberDetails(NumberType1 const realPart, NumberType2 const imaginaryPart);
 
-    AlbaNumber addBothIntegersAndReturnNumber(int64_t const integerValue1, int64_t const integerValue2) const;
+    AlbaNumber addBothIntegersAndReturnNumber(IntDataType const integerValue1, IntDataType const integerValue2) const;
     AlbaNumber addBothDoubleAndReturnNumber(double const doubleValue1, double const doubleValue2) const;
     AlbaNumber addBothFractionsAndReturnNumber(
         FractionData const& fractionData1, FractionData const& fractionData2) const;
-    AlbaNumber addIntegerAndDoubleAndReturnNumber(int64_t const integerValue, double const doubleValue) const;
-    AlbaNumber addIntegerAndFractionAndReturnNumber(int64_t const integerValue, FractionData const& fractionData) const;
+    AlbaNumber addIntegerAndDoubleAndReturnNumber(IntDataType const integerValue, double const doubleValue) const;
+    AlbaNumber addIntegerAndFractionAndReturnNumber(
+        IntDataType const integerValue, FractionData const& fractionData) const;
     AlbaNumber addFractionAndDoubleAndReturnNumber(FractionData const& fractionData, double const doubleValue) const;
-    AlbaNumber multiplyBothIntegersAndReturnNumber(int64_t const integerValue1, int64_t const integerValue2) const;
+    AlbaNumber multiplyBothIntegersAndReturnNumber(
+        IntDataType const integerValue1, IntDataType const integerValue2) const;
     AlbaNumber multiplyBothDoubleAndReturnNumber(double const doubleValue1, double const doubleValue2) const;
     AlbaNumber multiplyBothFractionsAndReturnNumber(
         FractionData const& fractionData1, FractionData const& fractionData2) const;
-    AlbaNumber multiplyIntegerAndDoubleAndReturnNumber(int64_t const integerValue, double const doubleValue) const;
+    AlbaNumber multiplyIntegerAndDoubleAndReturnNumber(IntDataType const integerValue, double const doubleValue) const;
     AlbaNumber multiplyIntegerAndFractionAndReturnNumber(
-        int64_t const integerValue, FractionData const& fractionData) const;
+        IntDataType const integerValue, FractionData const& fractionData) const;
     AlbaNumber multiplyFractionAndDoubleAndReturnNumber(
         FractionData const& fractionData, double const doubleValue) const;
-    AlbaNumber divideBothIntegersAndReturnNumber(int64_t const dividend, int64_t const divisor) const;
+    AlbaNumber divideBothIntegersAndReturnNumber(IntDataType const dividend, IntDataType const divisor) const;
     AlbaNumber divideDividendsAndDivisorsAndReturnNumber(
-        int64_t const dividendInteger, uint32_t const dividendUnsignedInteger, int64_t const divisorInteger,
-        uint32_t const divisorUnsignedInteger) const;
+        IntDataType const dividendInteger, DenominatorDataType const dividendUnsignedInteger,
+        IntDataType const divisorInteger, DenominatorDataType const divisorUnsignedInteger) const;
     AlbaNumber divideBothFractionsAndReturnNumber(
         FractionData const& dividendFractionData, FractionData const& divisorFractionData) const;
-    AlbaNumber raisePowerOfBothIntegersAndReturnNumber(int64_t const base, int64_t const exponent) const;
+    AlbaNumber raisePowerOfBothIntegersAndReturnNumber(IntDataType const base, IntDataType const exponent) const;
     AlbaNumber raisePowerOfFractionsAndIntegerAndReturnNumber(
-        FractionData const& baseFractionData, int64_t const exponent) const;
+        FractionData const& baseFractionData, IntDataType const exponent) const;
 
     friend std::ostream& operator<<(std::ostream& out, AlbaNumber const& number);
-
     Type m_type;  // Hotness: Type is much hotter.
     // use std variant instead? Nah, I dont wanna deal with getting the "index" to know the "type".
     NumberUnionData m_data;
-
     static_assert(sizeof(m_type) == 4, "The size of AlbaNumber type should be 4 bytes/32 bits.");
     static_assert(sizeof(m_data) == 8, "The size of AlbaNumber data should be 8 bytes/64 bits.");
 };
@@ -200,14 +192,12 @@ static_assert(sizeof(AlbaNumber) == 16, "The size of AlbaNumber should be 16 byt
 // Source: https://en.cppreference.com/w/cpp/language/user_literal
 // NOTE: The string needs to have a underscore '_' prefix because all letters as prefix are reserved.
 constexpr AlbaNumber operator"" _AS_ALBA_NUMBER(unsigned long long int const value) {
-    return AlbaNumber(static_cast<int64_t>(value));
+    return AlbaNumber(static_cast<AlbaNumber::IntDataType>(value));
 }
 constexpr AlbaNumber operator"" _AS_ALBA_NUMBER(long double const value) {
-    return AlbaNumber(static_cast<double>(value));
-}
+    return AlbaNumber(static_cast<double>(value));}
 // AlbaNumber operator "" _AS_ALBA_NUMBER(char const value) = delete;
 // not needed to delete because there is no implicit conversion
-
 template <>
 AlbaNumber::ConfigurationDetails getDefaultConfigurationDetails<AlbaNumber::ConfigurationDetails>();
 
