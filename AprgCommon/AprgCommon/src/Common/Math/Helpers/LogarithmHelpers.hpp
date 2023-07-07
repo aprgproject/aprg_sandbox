73e06be6d63938a4050f0d12ebd863765c865d6f
mevalba <markearvinalba@gmail.com>
2023-07-07 17:46:47 +0800

    Enhance APRG common module

--- AprgCommon/AprgCommon/src/Common/Math/Helpers/LogarithmHelpers.hpp
+++ AprgCommon/AprgCommon/src/Common/Math/Helpers/LogarithmHelpers.hpp
@@ -11,20 +11,18 @@ inline double getLogarithm(double const base, double const inputForLogarithm) {
     // change of base formula
     // log a(b) = C
     // -> b = a^C
-    // -> log10(b) = log10(a^C)
-    // -> log10(b) = C*log10(a)
-    // -> C = log10(b)/log10(a)
+    // -> ln(b) = ln(a^C)
+    // -> ln(b) = C*ln(a)
+    // -> C = ln(b)/ln(a)
 
-    // log a(b) = log10(b) / log10(a)
-    return log10(inputForLogarithm) / log10(base);
+    // log a(b) = ln(b) / ln(a)
+    return log(inputForLogarithm) / log(base);
 }
 
-template <typename NumberType>
-inline NumberType getLogarithmWithBase2Of(NumberType const value) {
+template <typename NumberType>inline NumberType getLogarithmWithBase2Of(NumberType const value) {
     static_assert(typeHelper::isIntegralType<NumberType>(), "Number type must be an integer");
 
-    return AlbaBitValueUtilities<NumberType>::getLogarithmWithBase2Of(value);
-}
+    return AlbaBitValueUtilities<NumberType>::getLogarithmWithBase2Of(value);}
 
 template <typename NumberType>
 NumberType getLogarithmForIntegers(NumberType const base, NumberType const inputForLogarithm) {
