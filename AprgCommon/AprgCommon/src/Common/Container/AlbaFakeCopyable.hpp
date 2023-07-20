6db86ee8562fd4e46b46ebce7132638adcd33113
mevalba <markearvinalba@gmail.com>
2023-07-21 04:23:45 +0800

    Enhance APRG common module structure

--- AprgCommon/AprgCommon/src/Common/Container/AlbaFakeCopyable.hpp
+++ AprgCommon/AprgCommon/src/Common/Container/AlbaFakeCopyable.hpp
@@ -13,19 +13,17 @@ public:
 
     AlbaFakeCopyable(ObjectType const& object) : m_object(object) {}
 
-    AlbaFakeCopyable(AlbaFakeCopyable const&)  // copy constructor calls default constructor
-        : m_object() {}
+    // copy constructor calls default constructor
+    AlbaFakeCopyable(AlbaFakeCopyable const&) : m_object() {}
 
-    AlbaFakeCopyable& operator=(AlbaFakeCopyable const&)  // copy assignment calls default constructor
-    {}
+    // copy assignment calls default constructor
+    AlbaFakeCopyable& operator=(AlbaFakeCopyable const&) { return *this; }
 
     AlbaFakeCopyable(AlbaFakeCopyable&&) = default;
     AlbaFakeCopyable& operator=(AlbaFakeCopyable&&) = default;
-
     ObjectType const& getObject() const { return m_object; }
 
     ObjectType& getObjectReference() { return m_object; }
-
 private:
     friend std::ostream& operator<<(std::ostream& out, AlbaFakeCopyable<ObjectType> const& object) {
         out << object.m_object;
