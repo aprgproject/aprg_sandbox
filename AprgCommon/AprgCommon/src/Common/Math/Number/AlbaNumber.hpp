ae387967f9f0ea47dcf67c900a050f6f846f2c67
mevalba <markearvinalba@gmail.com>
2023-07-28 01:03:12 +0800

    Refine APRG common module structure

--- AprgCommon/AprgCommon/src/Common/Math/Number/AlbaNumber.hpp
+++ AprgCommon/AprgCommon/src/Common/Math/Number/AlbaNumber.hpp
@@ -77,14 +77,12 @@ public:
         static_assert(sizeof(ArithmeticType) <= 8, "Maximum size is 8 bytes/64 bits.");
         static_assert(
             !(sizeof(ArithmeticType) == 8 && typeHelper::isUnsignedType<ArithmeticType>()),
-            "Unsigned integer with 8 bytes/64 bits are not supported.");
+            "Unsigned integers with 8 bytes/64 bits are not supported.");
     }
 
-    template <typename ArithmeticType>
-    Type constexpr getTypeBasedFromArithmeticType() {
+    template <typename ArithmeticType>    Type constexpr getTypeBasedFromArithmeticType() {
         return typeHelper::isIntegralType<ArithmeticType>() ? Type::Integer : Type::Double;
     }
-
     // no need to be explicit in the constructors (allow implicit conversions)
 
     constexpr AlbaNumber() : m_type(Type::Integer), m_data() {}
