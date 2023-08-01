dcd15fbde36ad8c936a41c695a3f3e73610be309
mevalba <markearvinalba@gmail.com>
2023-08-01 20:34:28 +0800

    Refactor aprgbitmap and include JSON files

--- AprgBitmap/AprgBitmap/cmake/FilesAndDirectories.cmake
+++ AprgBitmap/AprgBitmap/cmake/FilesAndDirectories.cmake
@@ -1,14 +1,9 @@
-include(${APRG_DIR}/AprgCMakeHelpers/FileAndDirectoryMacros.cmake)
-include(${APRG_DIR}/AprgCMakeHelpers/PrintMacros.cmake)
-
 set(APRG_BITMAP_SOURCE_DIRECTORY
     ${APRG_BITMAP_DIR}/src
 )
-
 set(APRG_BITMAP_TEST_DIRECTORY
     ${APRG_BITMAP_DIR}/tst
 )
-
 set(APRG_BITMAP_INCLUDE_DIRECTORIES
     ${APRG_BITMAP_SOURCE_DIRECTORY}
 )
