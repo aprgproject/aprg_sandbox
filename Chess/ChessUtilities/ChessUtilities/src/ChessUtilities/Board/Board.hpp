0ff1dc56b3892ce33edebfff5fbaeec8f7142c71
mevalba <markearvinalba@gmail.com>
2023-08-15 14:40:54 +0800

    Refactor chessutilities module

--- Chess/ChessUtilities/ChessUtilities/src/ChessUtilities/Board/Board.hpp
+++ Chess/ChessUtilities/ChessUtilities/src/ChessUtilities/Board/Board.hpp
@@ -7,24 +7,22 @@
 #include <array>
 #include <cstdint>
 #include <functional>
+#include <limits>
 #include <optional>
 #include <vector>
-
 namespace alba {
 
 namespace chess {
-
 struct Board {
 public:
     static constexpr int CHESS_SIDE_SIZE = 8;
-    static constexpr int MAX_NUMBER_OF_MOVES = std::numeric_limits<int>::max();
+    //Conflict between std::numeric_limits::max() and the max(a, b) macro defined in minwindef.h included as part of Windows.h.
+    static constexpr int MAX_NUMBER_OF_MOVES = (std::numeric_limits<int>::max)();
     using PieceGrid = std::array<Piece, CHESS_SIDE_SIZE * CHESS_SIDE_SIZE>;
     using CoordinateCondition = std::function<bool(Coordinate const&)>;
-
     enum class CastleType { NotACastle, KingSideCastle, QueenSideCastle };
 
-    struct DeltaRange {
-        CoordinateDataType interval;
+    struct DeltaRange {        CoordinateDataType interval;
         CoordinateDataType invalidDelta;
     };
 
