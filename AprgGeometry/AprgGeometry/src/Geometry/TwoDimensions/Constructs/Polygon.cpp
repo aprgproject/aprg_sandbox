bd985372168a132003fd1fa497af0cb90d11a27d
mevalba <markearvinalba@gmail.com>
2023-08-25 00:16:10 +0800

    Address aprggeometry issues

--- AprgGeometry/AprgGeometry/src/Geometry/TwoDimensions/Constructs/Polygon.cpp
+++ AprgGeometry/AprgGeometry/src/Geometry/TwoDimensions/Constructs/Polygon.cpp
@@ -91,15 +91,13 @@ AlbaAngles Polygon<numberOfVertices>::getAnglesAtVertices() const {
         anglesAtVertices.emplace_back(
             getTheInnerAngleUsingThreePoints(m_vertices[i], m_vertices[i - 1], m_vertices[i + 1]));
     }
-    anglesAtVertices.emplace_back(getTheInnerAngleUsingThreePoints(
-        m_vertices[sizeMinusOne], m_vertices[sizeMinusOne - 1], m_vertices[0]));
+    anglesAtVertices.emplace_back(
+        getTheInnerAngleUsingThreePoints(m_vertices[sizeMinusOne], m_vertices[sizeMinusOne - 1], m_vertices[0]));
     return anglesAtVertices;  // RVO
 }
-
 template <int numberOfVertices>
 AlbaAngle Polygon<numberOfVertices>::getSumOfAngles() const {
-    return AlbaAngle(AngleUnitType::Degrees, (numberOfVertices - 2) * 180);
-}
+    return AlbaAngle(AngleUnitType::Degrees, (numberOfVertices - 2) * 180);}
 
 template <int numberOfVertices>
 Points Polygon<numberOfVertices>::getPoints(double const interval) const {
@@ -122,6 +120,9 @@ void Polygon<numberOfVertices>::getPointsFromVerticesWithoutLastPoint(
     points.reserve(pointsFromCurrentLine.size());
     copy(pointsFromCurrentLine.cbegin(), pointsFromCurrentLine.cend(), back_inserter(points));
 }
+
+template class Polygon<3>;  // clang needs this on implementation file
+template class Polygon<4>;  // clang needs this on implementation file
 
 }  // namespace TwoDimensions
 }  // namespace alba
