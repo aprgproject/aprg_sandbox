9c61369bf2a76f2597ed69446802d65bb0a6b123
mevalba <markearvinalba@gmail.com>
2023-08-25 18:03:17 +0800

    Rectify common scripts and JSON files in AllCommonScripts

--- AllCommonScripts/CopyFileScripts/.vscode/locateAprgDirectory.sh
+++ AllCommonScripts/CopyFileScripts/.vscode/locateAprgDirectory.sh
@@ -4,26 +4,24 @@ findAprgDirectory() {
 	# Set variable values
 	aprgDirectory=""
 	local aprgDirectoryName="aprg"
-	local inputDirectory=$1
+	local inputDirectory=$(realpath "$1")
 	local localScriptName="locateAprgDirectory"
 	
 	# Validate input
 	if ! [ -d "$inputDirectory" ]; then
-		echo "$localScriptName:$LINENO The input directory [$inputDirectory] is not a directory"
+		echo "$localScriptName:$LINENO: The input directory [$inputDirectory] is not a directory"
 		return 1
 	fi
 	
 	# Display variable values
-	echo "$localScriptName:$LINENO The aprg directory name is [$aprgDirectoryName]."
-	echo "$localScriptName:$LINENO The input directory is [$inputDirectory] for searching [$aprgDirectoryName] directory."
+	echo "$localScriptName:$LINENO: The aprg directory name is [$aprgDirectoryName]."
+	echo "$localScriptName:$LINENO: The input directory is [$inputDirectory] for searching [$aprgDirectoryName] directory."
 	
 	# Set variables for search
-	searchingDirectory="$inputDirectory"
-	
+	searchingDirectory="$inputDirectory"	
 	# Loop until we reach the root directory ("/")
 	while [ "$searchingDirectory" != "/" ]; do
-		# Check if the directory exists in the current directory
-		if [ -d "$searchingDirectory/$aprgDirectoryName" ]; then
+		# Check if the directory exists in the current directory		if [ -d "$searchingDirectory/$aprgDirectoryName" ]; then
 			aprgDirectory="$searchingDirectory/$aprgDirectoryName"
 			break
 		else
@@ -34,9 +32,9 @@ findAprgDirectory() {
 	
 	# Validate output
 	if [ "$searchingDirectory" == "/" ]; then
-		echo "$localScriptName:$LINENO The directory name [$aprgDirectoryName] is not found in any parent directories of [$inputDirectory]."
+		echo "$localScriptName:$LINENO: The directory name [$aprgDirectoryName] is not found in any parent directories of [$inputDirectory]."
 		return 1
 	fi
 	
-	echo "$localScriptName:$LINENO The aprg directory is [$aprgDirectory]."
+	echo "$localScriptName:$LINENO: The aprg directory is [$aprgDirectory]."
 }
