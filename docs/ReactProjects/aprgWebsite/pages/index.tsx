97f43db20384deb3f443068a4685067e9d231c1b
mevalba <markearvinalba@gmail.com>
2023-08-26 07:03:47 +0800

    Rectify issues in aprgwebsite

--- docs/ReactProjects/aprgWebsite/pages/index.tsx
+++ docs/ReactProjects/aprgWebsite/pages/index.tsx
@@ -14,20 +14,20 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to APRG!
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          As for the goal of this project, it's pretty straightforward: There is NO goal! <br />
+          I'm an engineer who finds joy in crafting elegant solutions using clever abstractions. <br />
+          Simplicity is my mantra, and I strive to create code that is both easy to understand and efficient. <br />
+          In this project, there are no rigid rules or strict restrictions – it's all about having a good time while coding! <br />
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div className={styles.grid}>          <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
